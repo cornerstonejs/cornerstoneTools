7c9bc042255294c6e22b82ca1b9794d0a2f7b8fe
Chris Hafey <chafey@gmail.com>
2014-04-09 10:47:31 -0500

    Updated stackScroll to work a bit smarter and be less sensitive Updated to newer version of cornerstone The stack demo now shows the current image # along with total # images in the stack

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -1,4 +1,4 @@
-/*! cornerstoneTools - v0.0.1 - 2014-04-08 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
+/*! cornerstoneTools - v0.0.1 - 2014-04-09 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
 // Begin Source: src/inputSources/mouseWheelInput.js
 var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
 
@@ -2197,7 +2197,16 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
     {
         var mouseData = e.originalEvent.detail;
         if(cornerstoneTools.isMouseButtonEnabled(mouseData.which, e.data.mouseButtonMask)) {
-            $(mouseData.element).on("CornerstoneToolsMouseDrag", mouseDragCallback);
+            var toolData = cornerstoneTools.getToolState(mouseData.element, 'stack');
+            if(toolData === undefined || toolData.data === undefined || toolData.data.length === 0) {
+                return;
+            }
+            var stackData = toolData.data[0];
+
+            var eventData = {
+                deltaY : 0
+            };
+            $(mouseData.element).on("CornerstoneToolsMouseDrag", eventData, mouseDragCallback);
             $(mouseData.element).on("CornerstoneToolsMouseUp", mouseUpCallback);
             return false; // false = cases jquery to preventDefault() and stopPropagation() this event
         }
@@ -2206,9 +2215,42 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
     function mouseDragCallback(e)
     {
         var mouseMoveData = e.originalEvent.detail;
+        var eventData = e.detail;
+        var stackData = cornerstoneTools.getToolState(mouseMoveData.element, 'stack');
+        if(stackData === undefined || stackData.data === undefined || stackData.data.length === 0) {
+            return;
+        }
+        var stack = stackData.data[0];
 
-        var images = mouseMoveData.deltaPoints.page.y;
-        scroll(mouseMoveData.element, images);
+        var toolData = cornerstoneTools.getToolState(mouseMoveData.element, toolType);
+        if(toolData === undefined || toolData.data === undefined || toolData.data.length === 0) {
+            toolData = {
+                deltaY : 0
+            };
+            cornerstoneTools.addToolState(mouseMoveData.element, toolType, toolData);
+        }
+        else
+        {
+            toolData = toolData.data[0];
+        }
+
+        toolData.deltaY += mouseMoveData.deltaPoints.page.y;
+        if(toolData.deltaY >=3 || toolData.deltaY <= -3)
+        {
+            var imageDelta = toolData.deltaY / 3;
+            var imageDeltaMod = toolData.deltaY % 3;
+            var imageIdIndexOffset = Math.round(imageDelta);
+            toolData.deltaY = imageDeltaMod;
+            var imageIdIndex = stack.currentImageIdIndex + imageIdIndexOffset;
+            imageIdIndex = Math.min(stack.imageIds.length - 1, imageIdIndex);
+            imageIdIndex = Math.max(0, imageIdIndex);
+            if(imageIdIndex !== stack.currentImageIdIndex)
+            {
+                stack.currentImageIdIndex = imageIdIndex;
+                cornerstone.newStackImage(mouseMoveData.element, stack.imageIds[imageIdIndex]);
+            }
+
+        }
 
         return false; // false = cases jquery to preventDefault() and stopPropagation() this event
     }
--- src/stackTools/stackScroll.js
+++ src/stackTools/stackScroll.js
@@ -39,7 +39,16 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
     {
         var mouseData = e.originalEvent.detail;
         if(cornerstoneTools.isMouseButtonEnabled(mouseData.which, e.data.mouseButtonMask)) {
-            $(mouseData.element).on("CornerstoneToolsMouseDrag", mouseDragCallback);
+            var toolData = cornerstoneTools.getToolState(mouseData.element, 'stack');
+            if(toolData === undefined || toolData.data === undefined || toolData.data.length === 0) {
+                return;
+            }
+            var stackData = toolData.data[0];
+
+            var eventData = {
+                deltaY : 0
+            };
+            $(mouseData.element).on("CornerstoneToolsMouseDrag", eventData, mouseDragCallback);
             $(mouseData.element).on("CornerstoneToolsMouseUp", mouseUpCallback);
             return false; // false = cases jquery to preventDefault() and stopPropagation() this event
         }
@@ -48,9 +57,42 @@ var cornerstoneTools = (function ($, cornerstone, cornerstoneTools) {
     function mouseDragCallback(e)
     {
         var mouseMoveData = e.originalEvent.detail;
+        var eventData = e.detail;
+        var stackData = cornerstoneTools.getToolState(mouseMoveData.element, 'stack');
+        if(stackData === undefined || stackData.data === undefined || stackData.data.length === 0) {
+            return;
+        }
+        var stack = stackData.data[0];
 
-        var images = mouseMoveData.deltaPoints.page.y;
-        scroll(mouseMoveData.element, images);
+        var toolData = cornerstoneTools.getToolState(mouseMoveData.element, toolType);
+        if(toolData === undefined || toolData.data === undefined || toolData.data.length === 0) {
+            toolData = {
+                deltaY : 0
+            };
+            cornerstoneTools.addToolState(mouseMoveData.element, toolType, toolData);
+        }
+        else
+        {
+            toolData = toolData.data[0];
+        }
+
+        toolData.deltaY += mouseMoveData.deltaPoints.page.y;
+        if(toolData.deltaY >=3 || toolData.deltaY <= -3)
+        {
+            var imageDelta = toolData.deltaY / 3;
+            var imageDeltaMod = toolData.deltaY % 3;
+            var imageIdIndexOffset = Math.round(imageDelta);
+            toolData.deltaY = imageDeltaMod;
+            var imageIdIndex = stack.currentImageIdIndex + imageIdIndexOffset;
+            imageIdIndex = Math.min(stack.imageIds.length - 1, imageIdIndex);
+            imageIdIndex = Math.max(0, imageIdIndex);
+            if(imageIdIndex !== stack.currentImageIdIndex)
+            {
+                stack.currentImageIdIndex = imageIdIndex;
+                cornerstone.newStackImage(mouseMoveData.element, stack.imageIds[imageIdIndex]);
+            }
+
+        }
 
         return false; // false = cases jquery to preventDefault() and stopPropagation() this event
     }
