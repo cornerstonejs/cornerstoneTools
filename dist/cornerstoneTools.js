e39f46838d2153c459700ddd7aae66bce2b12fb2
Rion Williams <rionmonster@gmail.com>
2015-07-02 09:34:33 -0500

    Added stackScrollSynchronizer with Example
    Added the stackScrollSynchronizer tool along with an example of it's
    usage (needs review). Added triggers for the "CornerstoneImageScroll"
    evnet on any scrolling behavior to trigger the synchronizer if it is
    active.
    
    Conflicts:
    	dist/cornerstoneTools.js
    	dist/cornerstoneTools.min.js
    	src/synchronization/synchronizer.js

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -7591,6 +7591,71 @@ if (typeof cornerstoneTools === 'undefined') {
  
 // End Source; src/synchronization/stackImagePositionSynchronizer.js
 
+// Begin Source: src/synchronization/stackScrollSynchronizer.js
+// Begin Source: src/synchronization/stackScrollSynchronizer.js
+var cornerstoneTools = (function($, cornerstone, cornerstoneTools) {
+
+    'use strict';
+
+    if (cornerstoneTools === undefined) {
+        cornerstoneTools = {};
+    }
+
+    // This function causes any scrolling actions within the stack to propagate to 
+    // all of the other viewports that are synced
+    function stackScrollSynchronizer(synchronizer, sourceElement, targetElement, eventData) {
+
+        // If the target and source are the same, ignore
+        if (sourceElement === targetElement) {
+            // TODO: Look into incrementing here or pulling the previous index
+            //       as the built-in scrolling may take over
+
+            return;
+        }
+
+        // If there is no event, ignore synchronization
+        if (eventData === undefined) {
+            return;
+        }
+
+        // Target the stack of the target viewport
+        var targetStackToolDataSource = cornerstoneTools.getToolState(targetElement, 'stack');
+        var targetStackData = targetStackToolDataSource.data[0];
+
+        // It's not the same area, so scroll if able
+        var direction = eventData.direction;
+
+        // Get the current index for the stack
+        var newImageIdIndex = targetStackData.currentImageIdIndex;
+
+        // Update the position based on the direction
+        newImageIdIndex = (direction < 0) ? newImageIdIndex + 1 : newImageIdIndex - 1;
+
+        // Ensure the index does not exceed the bounds of the stack
+        newImageIdIndex = Math.min(Math.max(newImageIdIndex, 0), targetStackData.imageIds.length - 1);
+
+        // If the index has not changed, ignore it
+        if (targetStackData.currentImageIdIndex === newImageIdIndex) {
+            return;
+        }
+
+        // Otherwise load the image
+        cornerstone.loadAndCacheImage(targetStackData.imageIds[newImageIdIndex]).then(function(image) {
+            var viewport = cornerstone.getViewport(targetElement);
+            targetStackData.currentImageIdIndex = newImageIdIndex;
+            synchronizer.displayImage(targetElement, image, viewport);
+        });
+    }
+
+    // module/private exports
+    cornerstoneTools.stackScrollSynchronizer = stackScrollSynchronizer;
+
+    return cornerstoneTools;
+}($, cornerstone, cornerstoneTools));
+// End Source; src/synchronization/stackScrollSynchronizer.js
+ 
+// End Source; src/synchronization/stackScrollSynchronizer.js
+
 // Begin Source: src/synchronization/synchronizer.js
 (function($, cornerstone, cornerstoneTools) {
 
@@ -7606,23 +7671,22 @@ if (typeof cornerstoneTools === 'undefined') {
 
         var ignoreFiredEvents = false;
 
-        function fireEvent(sourceEnabledElement) {
+        function fireEvent(sourceEnabledElement, eventData) {
 
             // Broadcast an event that something changed
             ignoreFiredEvents = true;
             $.each(targetElements, function(index, targetEnabledElement) {
-                handler(that, sourceEnabledElement, targetEnabledElement);
+                handler(that, sourceEnabledElement, targetEnabledElement, eventData);
             });
             ignoreFiredEvents = false;
         }
 
-        function onEvent(e) {
+        function onEvent(e, eventData) {
             if (ignoreFiredEvents === true) {
-                //console.log("event ignored");
                 return;
             }
 
-            fireEvent(e.currentTarget);
+            fireEvent(e.currentTarget, eventData);
         }
 
         // adds an element as a source
---
+++ src/synchronization/stackScrollSynchronizer.js
@@ -0,0 +1,61 @@
+﻿// Begin Source: src/synchronization/stackScrollSynchronizer.js
+var cornerstoneTools = (function($, cornerstone, cornerstoneTools) {
+
+    'use strict';
+
+    if (cornerstoneTools === undefined) {
+        cornerstoneTools = {};
+    }
+
+    // This function causes any scrolling actions within the stack to propagate to 
+    // all of the other viewports that are synced
+    function stackScrollSynchronizer(synchronizer, sourceElement, targetElement, eventData) {
+
+        // If the target and source are the same, ignore
+        if (sourceElement === targetElement) {
+            // TODO: Look into incrementing here or pulling the previous index
+            //       as the built-in scrolling may take over
+
+            return;
+        }
+
+        // If there is no event, ignore synchronization
+        if (eventData === undefined) {
+            return;
+        }
+
+        // Target the stack of the target viewport
+        var targetStackToolDataSource = cornerstoneTools.getToolState(targetElement, 'stack');
+        var targetStackData = targetStackToolDataSource.data[0];
+
+        // It's not the same area, so scroll if able
+        var direction = eventData.direction;
+
+        // Get the current index for the stack
+        var newImageIdIndex = targetStackData.currentImageIdIndex;
+
+        // Update the position based on the direction
+        newImageIdIndex = (direction < 0) ? newImageIdIndex + 1 : newImageIdIndex - 1;
+
+        // Ensure the index does not exceed the bounds of the stack
+        newImageIdIndex = Math.min(Math.max(newImageIdIndex, 0), targetStackData.imageIds.length - 1);
+
+        // If the index has not changed, ignore it
+        if (targetStackData.currentImageIdIndex === newImageIdIndex) {
+            return;
+        }
+
+        // Otherwise load the image
+        cornerstone.loadAndCacheImage(targetStackData.imageIds[newImageIdIndex]).then(function(image) {
+            var viewport = cornerstone.getViewport(targetElement);
+            targetStackData.currentImageIdIndex = newImageIdIndex;
+            synchronizer.displayImage(targetElement, image, viewport);
+        });
+    }
+
+    // module/private exports
+    cornerstoneTools.stackScrollSynchronizer = stackScrollSynchronizer;
+
+    return cornerstoneTools;
+}($, cornerstone, cornerstoneTools));
+// End Source; src/synchronization/stackScrollSynchronizer.js
