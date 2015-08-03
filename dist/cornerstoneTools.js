b872df62b8a30d5ead4dc9edb89de3b2c0388006
Leonardo M. Ramé <martinrame@yahoo.com>
2015-08-03 06:39:24 -0300

    removed artifacts generated by merge

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-/*! cornerstoneTools - v0.6.2 - 2015-08-02 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
-=======
-/*! cornerstoneTools - v0.6.2 - 2015-07-30 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
->>>>>>> 8c0b693d648b67d20943c8e1c4519a2df14f5ad4
+/*! cornerstoneTools - v0.6.2 - 2015-08-03 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
 // Begin Source: src/header.js
 if (typeof cornerstone === 'undefined') {
     cornerstone = {};
