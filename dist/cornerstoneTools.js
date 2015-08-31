f6298f3f877b1e5aa997d1bfd45c6c93a5755e21
Erik Ziegler <erik.ziegler@ulg.ac.be>
2015-08-31 16:14:03 +0200

    A bit of cleanup to make the translation approach a new strategy

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -5051,6 +5051,24 @@ if (typeof cornerstoneTools === 'undefined') {
 
     var startPoints;
 
+    function changeViewportScale(viewport, ticks) {
+        var config = cornerstoneTools.zoom.getConfiguration();
+        var pow = 1.7;
+        
+        var oldFactor = Math.log(viewport.scale) / Math.log(pow);
+        var factor = oldFactor + ticks;
+        
+        var scale = Math.pow(pow, factor);
+        viewport.scale = scale;
+        
+        if (config.maxScale && scale > config.maxScale) {
+            viewport.scale = config.maxScale;
+        } else if (config.minScale && scale < config.minScale) {
+            viewport.scale = config.minScale;
+        }
+        return viewport;
+    }
+
     function boundPosition(position, width, height) {
         position.x = Math.max(position.x, 0);
         position.y = Math.max(position.y, 0);
@@ -5086,67 +5104,45 @@ if (typeof cornerstoneTools === 'undefined') {
         return shift;
     }
 
-    function defaultStrategy(element, viewport, ticks) {
+    function defaultStrategy(eventData, ticks) {
+        var element = eventData.element;
+
         // Calculate the new scale factor based on how far the mouse has changed
-        var config = cornerstoneTools.zoom.getConfiguration();
-
-        var pow = 1.7;
-        var oldFactor = Math.log(viewport.scale) / Math.log(pow);
-
-        var factor;
-        if (config && config.invert === true) {
-            factor = oldFactor - ticks;
-        } else {
-            factor = oldFactor + ticks;
-        }
-        
-        var scale = Math.pow(pow, factor);
-        viewport.scale = scale;
-        
-        if (config && config.maxScale && scale > config.maxScale) {
-            viewport.scale = config.maxScale;
-        } else if (config && config.minScale && scale < config.minScale) {
-            viewport.scale = config.minScale;
-        }
-
+        var viewport = changeViewportScale(eventData.viewport, ticks);
         cornerstone.setViewport(element, viewport);
-    }
-
-    function mouseUpCallback(e, eventData) {
-        $(eventData.element).off('CornerstoneToolsMouseDrag', mouseDragCallback);
-        $(eventData.element).off('CornerstoneToolsMouseUp', mouseUpCallback);
-    }
-
-    function mouseDownCallback(e, eventData) {
-        if (cornerstoneTools.isMouseButtonEnabled(eventData.which, e.data.mouseButtonMask)) {
-            startPoints = eventData.startPoints;
-            $(eventData.element).on('CornerstoneToolsMouseDrag', mouseDragCallback);
-            $(eventData.element).on('CornerstoneToolsMouseUp', mouseUpCallback);
-            return false; // false = cases jquery to preventDefault() and stopPropagation() this event
-        }
-    }
-
-    function mouseDragCallback(e, eventData) {
-        var ticks = eventData.deltaPoints.page.y / 100;
-        if (!ticks) {
-            return false;
-        }
-
-        cornerstoneTools.zoom.strategy(eventData.element, eventData.viewport, ticks);
 
         // Now that the scale has been updated, determine the offset we need to apply to the center so we can
         // keep the original start location in the same position
+        var newCoords = cornerstone.pageToPixel(element, eventData.startPoints.page.x, eventData.startPoints.page.y);
+        var shift = {
+            x: eventData.startPoints.image.x - newCoords.x,
+            y: eventData.startPoints.image.y - newCoords.y
+        };
+
+        shift = correctShift(shift, viewport);
+        viewport.translation.x -= shift.x;
+        viewport.translation.y -= shift.y;
+        cornerstone.setViewport(element, viewport);
+    }
+
+    function translateStrategy(eventData, ticks) {
+        var element = eventData.element;
+        var image = eventData.image;
+
+        // Calculate the new scale factor based on how far the mouse has changed
+        var viewport = changeViewportScale(eventData.viewport, ticks);
+        cornerstone.setViewport(element, viewport);
+
         var config = cornerstoneTools.zoom.getConfiguration();
         var shift,
             newCoords;
-        var viewport = eventData.viewport;
 
-        var outwardsTranslateSpeed = 10;
-        var inwardsTranslateSpeed = 10;
-        var outwardsMinScaleToTranslate = 5;
+        var outwardsTranslateSpeed = 8;
+        var inwardsTranslateSpeed = 8;
+        var outwardsMinScaleToTranslate = 3;
         var minTranslation = 0.01;
 
-        if (ticks < 0 && config && config.zoomOutFromCenter) {
+        if (ticks < 0) {
             // Zoom outwards from the image center
             shift = {
                 x: viewport.scale < outwardsMinScaleToTranslate ? viewport.translation.x / outwardsTranslateSpeed : 0,
@@ -5161,19 +5157,19 @@ if (typeof cornerstoneTools === 'undefined') {
                 shift.y = 0;
             } else if (Math.abs(viewport.translation.x) < minTranslation &&
                        Math.abs(viewport.translation.y) < minTranslation) {
-                cornerstone.setViewport(eventData.element, viewport);
+                cornerstone.setViewport(element, viewport);
                 return false;
             }
         } else {
-            newCoords = cornerstone.pageToPixel(eventData.element, startPoints.page.x, startPoints.page.y);
+            newCoords = cornerstone.pageToPixel(element, startPoints.page.x, startPoints.page.y);
             if (config && config.preventZoomOutsideImage) {
-                startPoints.image = boundPosition(startPoints.image, eventData.image.width, eventData.image.height);
-                newCoords = boundPosition(newCoords, eventData.image.width, eventData.image.height);
+                startPoints.image = boundPosition(startPoints.image, image.width, image.height);
+                newCoords = boundPosition(newCoords, image.width, image.height);
             }
             // Zoom inwards to the current image point
             var desiredTranslation = {
-                x: eventData.image.width / 2 - startPoints.image.x,
-                y: eventData.image.height / 2 - startPoints.image.y
+                x: image.width / 2 - startPoints.image.x,
+                y: image.height / 2 - startPoints.image.y
             };
 
             var distanceToDesired = {
@@ -5194,76 +5190,66 @@ if (typeof cornerstoneTools === 'undefined') {
                 shift.y = 0;
             } else if (Math.abs(distanceToDesired.x) < minTranslation &&
                        Math.abs(distanceToDesired.y) < minTranslation) {
-                cornerstone.setViewport(eventData.element, viewport);
+                cornerstone.setViewport(element, viewport);
                 return false;
             }
         }
 
         shift = correctShift(shift, viewport);
         if (!shift.x && !shift.y) {
-            return false; // false = causes jquery to preventDefault() and stopPropagation() this event
+            return false;
         }
 
         viewport.translation.x -= shift.x;
         viewport.translation.y -= shift.y;
-        cornerstone.setViewport(eventData.element, viewport);
+        cornerstone.setViewport(element, viewport);
+    }
+
+    function mouseUpCallback(e, eventData) {
+        $(eventData.element).off('CornerstoneToolsMouseDrag', dragCallback);
+        $(eventData.element).off('CornerstoneToolsMouseUp', mouseUpCallback);
+    }
+
+    function mouseDownCallback(e, eventData) {
+        if (cornerstoneTools.isMouseButtonEnabled(eventData.which, e.data.mouseButtonMask)) {
+            startPoints = eventData.startPoints; // Used for translateStrategy
+            $(eventData.element).on('CornerstoneToolsMouseDrag', dragCallback);
+            $(eventData.element).on('CornerstoneToolsMouseUp', mouseUpCallback);
+            return false; // false = cases jquery to preventDefault() and stopPropagation() this event
+        }
+    }
+
+    function dragCallback(e, eventData) {
+        if (!eventData.deltaPoints.page.y) {
+            return false;
+        }
+        var ticks = eventData.deltaPoints.page.y / 100;
+        cornerstoneTools.zoom.strategy(eventData, ticks);
         return false; // false = causes jquery to preventDefault() and stopPropagation() this event
     }
 
     function mouseWheelCallback(e, eventData) {
         var ticks = -eventData.direction / 4;
-        cornerstoneTools.zoom.strategy(eventData.element, eventData.viewport, ticks);
+        var viewport = changeViewportScale(eventData.viewport, ticks);
+        cornerstone.setViewport(eventData.element, viewport);
     }
 
     function touchPinchCallback(e, eventData) {
         var ticks = eventData.direction / 4;
-        var viewport = eventData.viewport;
-        var config = cornerstoneTools.zoom.getConfiguration();
-        var pow = 1.7;
-        
-        var oldFactor = Math.log(viewport.scale) / Math.log(pow);
-        var factor = oldFactor + ticks;
-        
-        var scale = Math.pow(pow, factor);
-        viewport.scale = scale;
-        
-        if (config.maxScale && scale > config.maxScale) {
-            viewport.scale = config.maxScale;
-        } else if (config.minScale && scale < config.minScale) {
-            viewport.scale = config.minScale;
-        }
-
+        var viewport = changeViewportScale(eventData.viewport, ticks);
         cornerstone.setViewport(eventData.element, viewport);
-    }
-
-    function zoomTouchDrag(e, eventData) {
-        var dragData = eventData;
-        var ticks = dragData.deltaPoints.page.y / 100;
-        cornerstoneTools.zoom.strategy(dragData.element, dragData.viewport, ticks);
-
-        // Now that the scale has been updated, determine the offset we need to apply to the center so we can
-        // keep the original start location in the same position
-        var newCoords = cornerstone.pageToPixel(dragData.element, dragData.startPoints.page.x, dragData.startPoints.page.y);
-        var shift = {
-            x: dragData.startPoints.image.x - newCoords.x, y: dragData.startPoints.image.y - newCoords.y
-        };
-
-        shift = correctShift(shift, dragData.viewport);
-        dragData.viewport.translation.x -= shift.x;
-        dragData.viewport.translation.y -= shift.y;
-        cornerstone.setViewport(dragData.element, dragData.viewport);
-        return false; // false = causes jquery to preventDefault() and stopPropagation() this event
     }
 
     cornerstoneTools.zoom = cornerstoneTools.simpleMouseButtonTool(mouseDownCallback);
     cornerstoneTools.zoom.strategies = {
-        default: defaultStrategy
+        default: defaultStrategy,
+        translate: translateStrategy
     };
     cornerstoneTools.zoom.strategy = defaultStrategy;
 
     cornerstoneTools.zoomWheel = cornerstoneTools.mouseWheelTool(mouseWheelCallback);
     cornerstoneTools.zoomTouchPinch = cornerstoneTools.touchPinchTool(touchPinchCallback);
-    cornerstoneTools.zoomTouchDrag = cornerstoneTools.touchDragTool(zoomTouchDrag);
+    cornerstoneTools.zoomTouchDrag = cornerstoneTools.touchDragTool(dragCallback);
 
 })($, cornerstone, cornerstoneTools);
  
