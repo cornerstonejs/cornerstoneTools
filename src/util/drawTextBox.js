4eea0cd15ef5e0980afcacca1dca2b635d938882
Erik Ziegler <erik.ziegler@ulg.ac.be>
2015-11-11 13:36:02 +0100

    Multi-line support for drawTextBox. Allow handles to be drawn independently

--- src/util/drawTextBox.js
+++ src/util/drawTextBox.js
@@ -2,28 +2,54 @@
 
     'use strict';
 
-    function drawTextBox(context, text, x, y, color) {
+    function drawTextBox(context, textLines, x, y, color) {
+        if (Object.prototype.toString.call(textLines) !== '[object Array]') {
+            textLines = [ textLines ];
+        }
+
         var padding = 5,
             font = cornerstoneTools.textStyle.getFont(),
             fontSize = cornerstoneTools.textStyle.getFontSize(),
             backgroundColor = cornerstoneTools.textStyle.getBackgroundColor();
 
         context.save();
-
-        // Get the text width in the current font
         context.font = font;
-        var width = context.measureText(text).width;
+        context.textBaseline = 'top';
+        context.strokeStyle = color;
+
+        // Find the longest text width in the array of text data
+        var maxWidth = 0;
+        textLines.forEach(function(text) {
+            // Get the text width in the current font
+            var width = context.measureText(text).width;
+
+            // Find the maximum with for all the text rows;
+            maxWidth = Math.max(maxWidth, width);
+        });
 
         // Draw the background box with padding
-        context.textBaseline = 'top';
         context.fillStyle = backgroundColor;
-        context.fillRect(x, y - fontSize, width + (padding * 2), fontSize + (padding * 2));
-        
-        // Draw the text
-        context.fillStyle = color;
-        context.fillText(text, x + padding, y - fontSize + padding);
-        
+
+        // Calculate the bounding box for this text box
+        var boundingBox = {
+            left: x,
+            top: y,
+            width: maxWidth + (padding * 2),
+            height: textLines.length * (fontSize + (padding * 2))
+        };
+        context.fillRect(boundingBox.left, boundingBox.top, boundingBox.width, boundingBox.height);
+
+        // Draw each of the text lines on top of the background box
+        textLines.forEach(function(text, index) {
+            context.fillStyle = color;
+            context.fillText(text, x + padding, y + fontSize * index + padding);
+
+        });
+
         context.restore();
+
+        // Return the bounding box so it can be used for pointNearHandle
+        return boundingBox;
     }
 
     // module exports
