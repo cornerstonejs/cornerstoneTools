1f129e1d8cda725a6f50ace89a4e9e388af2780a
chafey <chafey@gmail.com>
2016-08-22 15:14:45 -0700

    prefetcher now uses the type to determine the priority of the image load

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -1,4 +1,4 @@
-/*! cornerstoneTools - v0.7.8 - 2016-05-17 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
+/*! cornerstoneTools - v0.7.8 - 2016-08-22 | (c) 2014 Chris Hafey | https://github.com/chafey/cornerstoneTools */
 // Begin Source: src/header.js
 if (typeof cornerstone === 'undefined') {
     cornerstone = {};
@@ -7917,11 +7917,23 @@ if (typeof cornerstoneTools === 'undefined') {
                 return;
             }
 
+            function requestTypeToLoadPriority(requestDetails) {
+                if(requestDetails.type === 'prefetch') {
+                    return -5;
+                } else if(requestDetails.type === 'interactive') {
+                    return 0;
+                } else if(requestDetails.type === 'thumbnail') {
+                    return 5;
+                }
+            }
+
+            var priority = requestTypeToLoadPriority(requestDetails);
+
             var loader;
             if (requestDetails.preventCache === true) {
-                loader = cornerstone.loadImage(imageId);
+                loader = cornerstone.loadImage(imageId, {priority : priority});
             } else {
-                loader = cornerstone.loadAndCacheImage(imageId);
+                loader = cornerstone.loadAndCacheImage(imageId, {priority : priority});
             }
 
             // Load and cache the image
