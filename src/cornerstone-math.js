53ee703bba3565372a70574a02368a2ee1dfdeb8
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-09-01 14:32:25 +0200

    comment typo

--- src/cornerstone-math.js
+++ src/cornerstone-math.js
@@ -1,5 +1,5 @@
 /*
- * When loading sources direcly with <script type="module"> replace the line below with
+ * When loading sources directly with <script type="module"> replace the line below with
  *   export * from '../../cornerstoneMath/src/index.js';
  */
 
