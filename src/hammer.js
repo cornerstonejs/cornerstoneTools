9bfe8677fc28d39e691aace04b91536b2e26c6d6
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-09-04 08:46:43 +0200

    Add remarks in the top of jquery.js and hammer.js

--- src/hammer.js
+++ src/hammer.js
@@ -1,3 +1,7 @@
+/*
+ * When loading sources directly with <script type="module"> remove the line below
+ * (keep only the export line)
+ */
 import * as Hammer from 'hammerjs';
 
 export default Hammer;
