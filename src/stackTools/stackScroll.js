411ddefb85011f5a958baf0bd01e138c5292f63e
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2017-09-21 14:23:40 +0200

    _stackScroll.getConfiguration().loop instead of stackScroll.getConfiguration().scrollLoop _loop = false by default

--- src/stackTools/stackScroll.js
+++ src/stackTools/stackScroll.js
@@ -30,7 +30,15 @@ function mouseDownCallback (e, eventData) {
 function mouseWheelCallback (e, eventData) {
   const images = -eventData.direction;
 
-  scroll(eventData.element, images, stackScroll.getConfiguration().scrollLoop);
+  const config = stackScroll.getConfiguration();
+
+  let loop = false;
+
+  if (config && config.loop) {
+    loop = config.loop;
+  }
+
+  scroll(eventData.element, images, loop);
 }
 
 function dragCallback (e, eventData) {
