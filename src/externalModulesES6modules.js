090104809a79f681a3a3004cd85bcf43f96d63e6
kofifus <mikagenic@gmail.com>
2017-10-04 15:09:45 +1100

    external modules

--- src/externalModulesES6modules.js
+++ src/externalModulesES6modules.js
@@ -1,5 +1,6 @@
 import * as cornerstone from '../../cornerstone/src/index.js';
 import * as dicomParser from '../../dicomParser/src/index.js';
+import * as cornerstoneMath from '../../cornerstoneMath/src/index.js';
 const $ = window.$;
 const Hammer = window.Hammer;
 
