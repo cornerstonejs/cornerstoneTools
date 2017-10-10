aafb6d0b0c8fe5b4b0413895bea420935db6c587
Aloïs Dreyfus <alois.dreyfus@ido-in.com>
2017-10-10 11:03:38 +0200

    Feature/doneCallback uses recent viewport parameters (#251)
    * Do not pass the viewport parameters, because they may have changed between the time we request the scroll, and the effective execution of doneCallback.
    
    * Remove the useless variable 'viewport'

--- src/stackTools/playClip.js
+++ src/stackTools/playClip.js
@@ -168,7 +168,6 @@ function playClip (element, framesPerSecond) {
 
         // Hoisting of context variables
     let loader,
-      viewport,
       startLoadingHandler,
       endLoadingHandler,
       errorLoadingHandler,
@@ -208,8 +207,6 @@ function playClip (element, framesPerSecond) {
         startLoadingHandler(element);
       }
 
-      viewport = cornerstone.getViewport(element);
-
       if (stackData.preventCache === true) {
         loader = cornerstone.loadImage(stackData.imageIds[newImageIdIndex]);
       } else {
@@ -221,9 +218,9 @@ function playClip (element, framesPerSecond) {
           stackData.currentImageIdIndex = newImageIdIndex;
           if (stackRenderer) {
             stackRenderer.currentImageIdIndex = newImageIdIndex;
-            stackRenderer.render(element, stackToolData.data, viewport);
+            stackRenderer.render(element, stackToolData.data);
           } else {
-            cornerstone.displayImage(element, image, viewport);
+            cornerstone.displayImage(element, image);
           }
           if (endLoadingHandler) {
             endLoadingHandler(element, image);
