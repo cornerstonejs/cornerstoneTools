68b4ded02bcc670fec9ef10496d61a0758c7c82b
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2017-10-19 14:22:23 -0200

    OHIF-128: got externalModulesES6modules.js again

---
+++ src/externalModulesES6modules.js
@@ -0,0 +1,18 @@
+import * as cornerstone from '../../cornerstone/src/index.js';
+import * as dicomParser from '../../dicomParser/src/index.js';
+import * as cornerstoneMath from '../../cornerstoneMath/src/index.js';
+const $ = window.$;
+const Hammer = window.Hammer;
+
+let cornerstone;
+
+const external = {
+  set cornerstone (cs) {
+    cornerstone = cs;
+  },
+  get cornerstone () {
+    return cornerstone;
+  }
+};
+
+export { $, Hammer, external, cornerstoneMath };
