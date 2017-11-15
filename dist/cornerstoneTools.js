738ea370da3393d1b27094137b53c2b5ad4e273d
Erik Ziegler <erik.sweed@gmail.com>
2017-11-15 17:33:37 +0100

    Bump to version 1.0.3

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -1,4 +1,4 @@
-/*! cornerstone-tools - 1.0.2 - 2017-11-15 | (c) 2017 Chris Hafey | https://github.com/chafey/cornerstoneTools */
+/*! cornerstone-tools - 1.0.3 - 2017-11-15 | (c) 2017 Chris Hafey | https://github.com/chafey/cornerstoneTools */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory(require("cornerstone-math"));
@@ -16264,7 +16264,7 @@ exports.adaptiveBrush = adaptiveBrush;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = '1.0.2';
+exports.default = '1.0.3';
 
 /***/ })
 /******/ ]);
