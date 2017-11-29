a2efd5cb76784df1df5796f614fdab5178ec6d7e
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2017-11-29 15:35:31 -0200

    OHIF-184: build library

--- dist/cornerstoneTools.js
+++ dist/cornerstoneTools.js
@@ -1,4 +1,4 @@
-/*! cornerstone-tools - 1.1.2 - 2017-11-21 | (c) 2017 Chris Hafey | https://github.com/chafey/cornerstoneTools */
+/*! cornerstone-tools - 1.1.2 - 2017-11-29 | (c) 2017 Chris Hafey | https://github.com/chafey/cornerstoneTools */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
@@ -7408,8 +7408,14 @@ exports.default = function (synchronizer, sourceElement, targetElement) {
   var cornerstone = _externalModules2.default.cornerstone;
   var sourceImage = cornerstone.getEnabledElement(sourceElement).image;
   var sourceImagePlane = cornerstone.metaData.get('imagePlaneModule', sourceImage.imageId);
-  var sourceImagePosition = (0, _convertToVector2.default)(sourceImagePlane.imagePositionPatient);
 
+  if (sourceImagePlane === undefined || sourceImagePlane.imagePositionPatient === undefined) {
+    // Console.log('No position found for image ' + sourceImage.imageId);
+
+    return;
+  }
+
+  var sourceImagePosition = (0, _convertToVector2.default)(sourceImagePlane.imagePositionPatient);
   var stackToolDataSource = (0, _toolState.getToolState)(targetElement, 'stack');
   var stackData = stackToolDataSource.data[0];
 
@@ -7418,6 +7424,13 @@ exports.default = function (synchronizer, sourceElement, targetElement) {
 
   stackData.imageIds.forEach(function (imageId, index) {
     var imagePlane = cornerstone.metaData.get('imagePlaneModule', imageId);
+
+    if (imagePlane === undefined || imagePlane.imagePositionPatient === undefined) {
+      // Console.log('No position found for image ' + imageId);
+
+      return;
+    }
+
     var imagePosition = (0, _convertToVector2.default)(imagePlane.imagePositionPatient);
     var distance = imagePosition.distanceToSquared(sourceImagePosition);
     // Console.log(index + '=' + distance);
@@ -14182,7 +14195,7 @@ exports.default = function (element, filename, mimetype) {
   // / convert canvas content to data-uri for link. When download
   // / attribute is set the content pointed to by link will be
   // / pushed as 'download' in HTML5 capable browsers
-  lnk.href = canvas.toDataURL(mimetype);
+  lnk.href = canvas.toDataURL(mimetype, 1);
 
   // / create a 'fake' click-event to trigger the download
   if (document.createEvent) {
