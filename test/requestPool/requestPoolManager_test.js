b90df4ad145d3e1dd026e31d81bd8e860d80265c
DREYFUS ALOIS <alois.dreyfus@ido-in.com>
2018-03-27 11:05:29 +0200

    test putRequestPoolType()

--- test/requestPool/requestPoolManager_test.js
+++ test/requestPool/requestPoolManager_test.js
@@ -27,3 +27,43 @@ describe('addRequest', function () {
     expect(interactionStack[2].imageId).to.equal('request2');
   });
 });
+
+describe('putRequestPoolType', function () {
+  beforeEach(() => {
+  });
+
+  it('should correctly put and modify requestPool types', function () {
+    requestPoolManager.initDefaultRequestPoolTypes();
+    
+    const requestPoolTypes = requestPoolManager.getRequestPoolTypes();
+    
+    expect(requestPoolTypes[0].name).to.equal('interaction');
+    expect(requestPoolTypes[1].name).to.equal('thumbnail');
+    expect(requestPoolTypes[2].name).to.equal('prefetch');
+    
+    requestPoolManager.putRequestPoolType('thumbnail', 5, 0);
+    requestPoolManager.putRequestPoolType('autoPrefetch', 0, function () {
+      const maxSimultaneousRequests = cornerstoneTools.getMaxSimultaneousRequests();
+
+      return Math.max(maxSimultaneousRequests -1, 1);
+    });
+    requestPoolManager.putRequestPoolType('wadoRsSerie', 40, function () {
+      const maxSimultaneousRequests = cornerstoneTools.getMaxSimultaneousRequests();
+
+      return Math.max(maxSimultaneousRequests -1, 1);
+    });
+    requestPoolManager.putRequestPoolType('wadoRsSerie', 40, 2);
+    
+    expect(requestPoolTypes[0].name).to.equal('wadoRsSerie');
+    expect(requestPoolTypes[0].maxRequests).to.equal(2);
+    expect(requestPoolTypes[1].name).to.equal('interaction');
+    expect(requestPoolTypes[2].name).to.equal('prefetch');
+    expect(requestPoolTypes[3].name).to.equal('thumbnail');
+    expect(requestPoolTypes[3].maxRequests).to.equal(0);
+    expect(requestPoolTypes[4].name).to.equal('autoPrefetch');
+    
+    expect(function () {
+      requestPoolManager.putRequestPoolType('wadoRsSerie', 5, 2);
+    }).to.throw('Can\'t give priority 5 to requestPool type wadoRsSerie, because type thumbnail already has this priority');
+  });
+});
