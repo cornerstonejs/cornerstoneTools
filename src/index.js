964697c52bcc9eb6ebeae2855cec4a92120ac819
Gustavo Lelis <galelis@gmail.com>
2018-04-03 16:11:27 -0300

    Removing MagnifyOld

--- src/index.js
+++ src/index.js
@@ -122,7 +122,6 @@ export { default as imageStats } from './imageTools/imageStats.js';
 export { default as keyboardTool } from './imageTools/keyboardTool.js';
 export { length, lengthTouch } from './imageTools/length.js';
 export { magnify, magnifyTouchDrag } from './imageTools/magnify.js';
-export { magnifyOld, magnifyOldTouchDrag } from './imageTools/magnifyOld.js';
 export { default as mouseButtonRectangleTool } from './imageTools/mouseButtonRectangleTool.js';
 export { default as mouseButtonTool } from './imageTools/mouseButtonTool.js';
 export { default as mouseWheelTool } from './imageTools/mouseWheelTool.js';
