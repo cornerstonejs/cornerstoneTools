31c665ac4f4993748803f2f3c1656847fed3ddf3
LIUG <674762353@qq.com>
2018-04-23 15:25:00 +0800

    update: touchDragTool add event listener
    update: touchDragTool add event listener 'cornerstoneimagerendered' to execute strategy.

--- src/imageTools/dragProbe.js
+++ src/imageTools/dragProbe.js
@@ -214,6 +214,10 @@ function mouseDownCallback (e) {
 function imageRenderedCallback () {
   if (dragEventData) {
     dragProbe.strategy(dragEventData);
+    
+    if(dragEventData.element){
+       dragEventData.element.removeEventListener(EVENTS.IMAGE_RENDERED, imageRenderedCallback);
+    }
     dragEventData = null;
   }
 }
@@ -224,7 +228,9 @@ function imageRenderedCallback () {
 function dragCallback (e) {
   const eventData = e.detail;
   const element = eventData.element;
-
+  
+  element.addEventListener(EVENTS.IMAGE_RENDERED, imageRenderedCallback);
+  
   dragEventData = eventData;
   external.cornerstone.updateImage(element);
 
