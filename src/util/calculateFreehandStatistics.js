70b85fb46e1faae948d8108f35c9e1352d4b7b2c
James Petts <jamesapetts@gmail.com>
2018-05-02 14:03:17 +0100

    Freehand Update: drag to edit, passive functionality and bug fix for statistics (#392)
    * Drag functionality for freehand tool
    
    *  Fixed broken statistical calculation
    
    * Tidied up some styling issues
    
    * Fixed double call to endDrawing on disallowed drag attempt
    
    * Refactored freehand mouseUpCallback
    
    * Refactored freehand mouseDragCallback
    
    * Refactored freehand mouseDragCallBack some more to reduce cognitive complexity
    
    * Addressing code climate issues: reduced complexity of pointInFreehandROI
    
    * Addressing code climate issues: reduced complexity of getSum
    
    * fixed mouseMove re-enabling after mouseUp

--- src/util/calculateFreehandStatistics.js
+++ src/util/calculateFreehandStatistics.js
@@ -1,38 +1,56 @@
 import pointInFreehandROI from './pointInFreehandROI.js';
 
-const statisticsObj = {
-  count: 0,
-  mean: 0.0,
-  variance: 0.0,
-  stdDev: 0.0
-};
-
 export default function (sp, boundingBox, dataHandles) {
-  let sum = 0;
-  let sumSquared = 0;
+
+  const statisticsObj = {
+    count: 0,
+    mean: 0.0,
+    variance: 0.0,
+    stdDev: 0.0
+  };
+
+  const sum = getSum(sp, boundingBox, dataHandles);
+
+  if (sum.count === 0) {
+    return statisticsObj;
+  }
+
+  statisticsObj.count = sum.count;
+  statisticsObj.mean = sum.value / sum.count;
+  statisticsObj.variance = sum.squared / sum.count - statisticsObj.mean * statisticsObj.mean;
+  statisticsObj.stdDev = Math.sqrt(statisticsObj.variance);
+
+  return statisticsObj;
+}
+
+function getSum (sp, boundingBox, dataHandles) {
+  const sum = {
+    value: 0,
+    squared: 0,
+    count: 0
+  };
   let index = 0;
 
   for (let y = boundingBox.top; y < boundingBox.top + boundingBox.height; y++) {
     for (let x = boundingBox.left; x < boundingBox.left + boundingBox.width; x++) {
-      if (pointInFreehandROI(dataHandles, {
+      const point = {
         x,
         y
-      })) {
-        sum += sp[index];
-        sumSquared += sp[index] * sp[index];
-        statisticsObj.count++;
-      }
+      };
+
+      sumPointIfInFreehandROI(dataHandles, point, sum, sp[index]);
       index++;
     }
   }
 
-  if (statisticsObj.count === 0) {
-    return statisticsObj;
+  return sum;
+}
+
+
+function sumPointIfInFreehandROI (dataHandles, point, sum, pixelValue) {
+  if (pointInFreehandROI(dataHandles, point)) {
+    sum.value += pixelValue;
+    sum.squared += pixelValue * pixelValue;
+    sum.count++;
   }
-
-  statisticsObj.mean = sum / statisticsObj.count;
-  statisticsObj.variance = sumSquared / statisticsObj.count - statisticsObj.mean * statisticsObj.mean;
-  statisticsObj.stdDev = Math.sqrt(statisticsObj.variance);
-
-  return statisticsObj;
 }
--- src/util/pointInFreehandROI.js
+++ src/util/pointInFreehandROI.js
@@ -52,15 +52,21 @@ export default function (dataHandles, location) {
   let j = dataHandles.length - 1; // The last vertex is the previous one to the first
 
   for (let i = 0; i < dataHandles.length; i++) {
-    // Check if y values of line encapsulate location.y
-    if (isEnclosedY(location.y, dataHandles[i].y, dataHandles[j].y)) {
-      if (isLineRightOfPoint(location, dataHandles[i], dataHandles[j])) {
-        inROI = !inROI;
-      }
+    if (rayFromPointCrosssesLine(location, dataHandles[i], dataHandles[j])) {
+      inROI = !inROI;
     }
 
     j = i; // Here j is previous vertex to i
   }
 
   return inROI;
+}
+
+function rayFromPointCrosssesLine (point, handleI, handleJ) {
+  if (isEnclosedY(point.y, handleI.y, handleJ.y) && isLineRightOfPoint(point, handleI, handleJ)) {
+
+    return true;
+  }
+
+  return false;
 }
