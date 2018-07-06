9d388a59eb32ab2a1d16b45f0092bb852f7ee140
David Wire <dlwire@gmail.com>
2018-07-06 11:02:56 -0600

    make use of simple[MouseButton, Touch]Tool and update the example to enable tools not currently in use when eraser is activated

--- src/imageTools/eraser.js
+++ src/imageTools/eraser.js
@@ -1,37 +1,9 @@
 import * as cornerstoneTools from '../index.js';
-import EVENTS from '../events.js';
 import external from '../externalModules.js';
+import simpleMouseButtonTool from './simpleMouseButtonTool.js';
+import simpleTouchTool from './simpleTouchTool.js';
 
 const toolType = 'eraser';
-
-function eventListeners (event) {
-  function activate (element) {
-    element.addEventListener(event, deleteNearbyMeasurement);
-  }
-
-  function deactivate (element) {
-    element.removeEventListener(event, deleteNearbyMeasurement);
-  }
-
-  function disable (element) {
-    element.removeEventListener(event, deleteNearbyMeasurement);
-  }
-
-  function enable (element) {
-    element.removeEventListener(event, deleteNearbyMeasurement);
-  }
-
-  return {
-    activate,
-    deactivate,
-    disable,
-    enable
-  };
-}
-
-function pointNearTool () {
-  return false;
-}
 
 function deleteNearbyMeasurement (mouseEventData) {
   const coords = mouseEventData.detail.currentPoints.canvas;
@@ -52,21 +24,8 @@ function deleteNearbyMeasurement (mouseEventData) {
   });
 }
 
-const eraser = Object.assign({},
-  eventListeners(EVENTS.MOUSE_CLICK),
-  {
-    pointNearTool,
-    toolType
-  }
-);
-
-const eraserTouch = Object.assign({},
-  eventListeners(EVENTS.TOUCH_PRESS),
-  {
-    pointNearTool,
-    toolType
-  }
-);
+const eraser = simpleMouseButtonTool(deleteNearbyMeasurement, toolType);
+const eraserTouch = simpleTouchTool(deleteNearbyMeasurement, toolType);
 
 export {
   eraser,
