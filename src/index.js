7dbb01fffbd8be3577874ef8c28f531aa6285260
dannyrb <danny.ri.brown@gmail.com>
2018-07-16 16:34:48 -0400

    Drop seedPoints/seedAnnotate

--- src/index.js
+++ src/index.js
@@ -230,7 +230,6 @@ export { rotate, rotateTouchDrag } from './imageTools/rotate.js';
 export { default as rotateTouch } from './imageTools/rotateTouch.js';
 export { default as saveAs } from './imageTools/saveAs.js';
 export { default as scaleOverlayTool } from './imageTools/scaleOverlayTool.js';
-export { seedAnnotate, seedAnnotateTouch } from './imageTools/seedAnnotate.js';
 export { simpleAngle, simpleAngleTouch } from './imageTools/simpleAngle.js';
 export {
   default as simpleMouseButtonTool
