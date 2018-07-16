6be89cfb465e6aab40bd37315a7b06bb2388fade
dannyrb <danny.ri.brown@gmail.com>
2018-07-16 11:12:22 -0400

    test

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -1,15 +1,24 @@
 # reads version from package.json and sets prelease equal to Circle CI build number
 if [ $CI ] && [ $CIRCLE_BUILD_NUM ]
 then
-  newversion=$(echo $npm_package_version | sed -e "s/^\([0-9]*\.[0-9]*\.[0-9]*\-[a-z]*\).[0-9]*$/\1.$CIRCLE_BUILD_NUM/")
-  echo "Setting version to $newversion"
-  # uses npm-version to set version in package.json
-  # see https://docs.npmjs.com/cli/version
-  npm version $newversion --no-git-tag-version
-  # git add package.json
-  # git add package-lock.json
-  # git add .
-  # git commit -m "updated version to $newversion [ci skip]"
+	PACKAGE_VERSION=$(cat package.json \
+		| grep version \
+		| head -1 \
+		| awk -F: '{ print $2 }' \
+		| sed 's/[",]//g' \
+		| tr -d '[[:space:]]')
+
+	echo "Found package version: $NEW_PACKAGE_VERSION"
+
+	NEW_PACKAGE_VERSION=$(echo $PACKAGE_VERSION | sed -e "s/^\([0-9]*\.[0-9]*\.[0-9]*\-[a-z]*\).[0-9]*$/\1.$CIRCLE_BUILD_NUM/")
+	echo "Setting version to: $NEW_PACKAGE_VERSION"
+	# uses npm-version to set version in package.json
+	# see https://docs.npmjs.com/cli/version
+	npm version $NEW_PACKAGE_VERSION --no-git-tag-version
+	# git add package.json
+	# git add package-lock.json
+	# git add .
+	# git commit -m "updated version to $newversion [ci skip]"
 else
-  echo "Don't forget to update the build version!"
+  	echo "Don't forget to update the build version!"
 fi
