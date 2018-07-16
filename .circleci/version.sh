c984c9339945e0590b6a3da9f29ca6f52f97ff08
dannyrb <danny.ri.brown@gmail.com>
2018-07-16 10:12:35 -0400

    fancier version bump

---
+++ .circleci/version.sh
@@ -0,0 +1,13 @@
+# reads version from package.json and sets prelease equal to Circle CI build number
+if [ $CI ] && [ $CIRCLE_BUILD_NUM ]
+then
+  newversion=$(echo $npm_package_version | sed -e "s/^\([0-9]*\.[0-9]*\.[0-9]*\-[a-z]*\).[0-9]*$/\1.$CIRCLE_BUILD_NUM/")
+  echo "Setting version to $newversion"
+  # uses npm-version to set version in package.json
+  # see https://docs.npmjs.com/cli/version
+  npm version $newversion --no-git-tag-version
+  git add package.json
+  git commit -m "updated version to $newversion [ci skip]"
+else
+  echo "Don't forget to update the build version!"
+fi
