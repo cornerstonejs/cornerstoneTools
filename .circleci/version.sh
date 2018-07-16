227f945897e0e8e9218529a5125bb307c19376f1
dannyrb <danny.ri.brown@gmail.com>
2018-07-16 10:34:11 -0400

    Add all modified files

--- .circleci/version.sh
+++ .circleci/version.sh
@@ -6,8 +6,9 @@ then
   # uses npm-version to set version in package.json
   # see https://docs.npmjs.com/cli/version
   npm version $newversion --no-git-tag-version
-  git add package.json
-  git add package-lock.json
+  # git add package.json
+  # git add package-lock.json
+  git add .
   git commit -m "updated version to $newversion [ci skip]"
 else
   echo "Don't forget to update the build version!"
