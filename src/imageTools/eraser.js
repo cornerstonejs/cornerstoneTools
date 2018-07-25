3a37206f025f67fe848920e0cb70923b0d034596
David Wire <dlwire@gmail.com>
2018-07-25 01:19:38 -0600

    fix(eraser) Increment on the eraser tool, delete one piece of data at a time, highlight only the data which will be deleted (#457)

--- src/imageTools/eraser.js
+++ src/imageTools/eraser.js
@@ -1,31 +1,117 @@
 import * as cornerstoneTools from '../index.js';
 import external from '../externalModules.js';
-import simpleMouseButtonTool from './simpleMouseButtonTool.js';
+import EVENTS from '../events.js';
+import { setToolOptions } from '../toolOptions.js';
 import simpleTouchTool from './simpleTouchTool.js';
 
 const toolType = 'eraser';
+let configuration = {
+  supportedTools: {}
+};
 
-function deleteNearbyMeasurement (mouseEventData) {
-  const coords = mouseEventData.detail.currentPoints.canvas;
-  const element = mouseEventData.detail.element;
-
+function populateSupportedTools () {
+  configuration.supportedTools = {};
   Object.keys(cornerstoneTools).forEach(function (toolName) {
     const tool = cornerstoneTools[toolName]; // eslint-disable-line import/namespace
-    const toolState = cornerstoneTools.getToolState(element, toolName);
 
-    if (toolState) {
-      toolState.data.forEach(function (data) {
-        if(typeof tool.pointNearTool === 'function' &&
-          tool.pointNearTool(element, data, coords)) {
-          cornerstoneTools.removeToolState(element, toolName, data);
-          external.cornerstone.updateImage(element);
-        }
-      });
+    if (typeof tool.pointNearTool === 'function') {
+      configuration.supportedTools[toolName] = tool;
     }
   });
 }
 
-const eraser = simpleMouseButtonTool(deleteNearbyMeasurement, toolType);
+function deleteNearbyMeasurement (mouseEventData) {
+  const coords = mouseEventData.detail.currentPoints.canvas;
+  const element = mouseEventData.detail.element;
+  let foundDataToDelete = false;
+  let imageNeedsUpdate = false;
+
+  Object.entries(configuration.supportedTools).forEach(function ([toolName, tool]) {
+    const toolState = cornerstoneTools.getToolState(element, toolName);
+
+    if (toolState) {
+      toolState.data.forEach(function (data) {
+        if (!foundDataToDelete && tool.pointNearTool(element, data, coords)) {
+          cornerstoneTools.removeToolState(element, toolName, data);
+          foundDataToDelete = true;
+          imageNeedsUpdate = true;
+        }
+      });
+    }
+  });
+
+  if (imageNeedsUpdate) {
+    external.cornerstone.updateImage(element);
+  }
+}
+
+function highlightNearbyMeasurement (mouseEventData) {
+  const coords = mouseEventData.detail.currentPoints.canvas;
+  const element = mouseEventData.detail.element;
+  let imageNeedsUpdate = false;
+  let foundFirstActive = false;
+
+  Object.entries(configuration.supportedTools).forEach(function ([toolName, tool]) {
+    const toolState = cornerstoneTools.getToolState(element, toolName);
+
+    if (toolState) {
+      toolState.data.forEach(function (data) {
+        if (data.active) {
+          data.active = false;
+          imageNeedsUpdate = true;
+        }
+
+        if (!foundFirstActive && tool.pointNearTool(element, data, coords)) {
+          data.active = true;
+          imageNeedsUpdate = true;
+          foundFirstActive = true;
+        }
+      });
+    }
+  });
+
+  if (imageNeedsUpdate) {
+    external.cornerstone.updateImage(element);
+  }
+}
+
+const eraser = {
+  activate (element, mouseButtonMask, options = {}) {
+    options.mouseButtonMask = mouseButtonMask;
+    setToolOptions(toolType, element, options);
+
+    element.removeEventListener(EVENTS.MOUSE_DOWN_ACTIVATE, deleteNearbyMeasurement);
+    element.removeEventListener(EVENTS.MOUSE_MOVE, highlightNearbyMeasurement);
+
+    element.addEventListener(EVENTS.MOUSE_DOWN_ACTIVATE, deleteNearbyMeasurement);
+    element.addEventListener(EVENTS.MOUSE_MOVE, highlightNearbyMeasurement);
+
+    populateSupportedTools();
+  },
+
+  disable (element) {
+    element.removeEventListener(EVENTS.MOUSE_DOWN_ACTIVATE, deleteNearbyMeasurement);
+    element.removeEventListener(EVENTS.MOUSE_MOVE, highlightNearbyMeasurement);
+  },
+
+  enable (element) {
+    element.removeEventListener(EVENTS.MOUSE_DOWN_ACTIVATE, deleteNearbyMeasurement);
+    element.removeEventListener(EVENTS.MOUSE_MOVE, highlightNearbyMeasurement);
+  },
+
+  deactivate (element) {
+    element.removeEventListener(EVENTS.MOUSE_DOWN_ACTIVATE, deleteNearbyMeasurement);
+    element.removeEventListener(EVENTS.MOUSE_MOVE, highlightNearbyMeasurement);
+  },
+
+  getConfiguration () {
+    return configuration;
+  },
+
+  setConfiguration (config) {
+    configuration = config;
+  }
+};
 const eraserTouch = simpleTouchTool(deleteNearbyMeasurement, toolType);
 
 export {
