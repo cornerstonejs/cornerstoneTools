7c79833843f66b0d53917bb9ee0d889ba7cafaca
dannyrb <danny.ri.brown@gmail.com>
2018-07-27 11:27:38 -0400

    Replace touch drag with a bound alternative

--- src/eventDispatchers/touchEventHandlers/index.js
+++ src/eventDispatchers/touchEventHandlers/index.js
@@ -1,11 +1,14 @@
 import customCallbackHandler from './shared/customCallbackHandler.js';
 import onImageRendered from './onImageRendered.js';
 import tap from './tap.js';
-import touchDrag from './touchDrag.js';
 import touchStart from './touchStart.js';
 import touchStartActive from './touchStartActive.js';
 
 const doubleTap = customCallbackHandler.bind(null, 'doubleTapCallback');
+// TODO: some touchDrag tools don't want to fire on touchStart
+// TODO: Drag tools have an option `fireOnTouchStart` used to filter
+// TODO: Them out of TOUCH_START handler
+const touchDrag = customCallbackHandler.bind(null, 'touchDragCallback');
 const touchEnd = customCallbackHandler.bind(null, 'touchEndCallback');
 const touchPinch = customCallbackHandler.bind(null, 'touchPinchCallback');
 const touchPress = customCallbackHandler.bind(null, 'touchPressCallback');
