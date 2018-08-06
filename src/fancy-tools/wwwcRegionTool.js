178b69325de3ecef88d8b96f7caa9b74b128b319
dannyrb <danny.ri.brown@gmail.com>
2018-08-06 15:11:57 -0400

    Remove strategies, and move implementation details to bottom of file

--- src/fancy-tools/wwwcRegionTool.js
+++ src/fancy-tools/wwwcRegionTool.js
@@ -9,85 +9,10 @@ import clip from '../util/clip.js';
 import getLuminance from '../util/getLuminance.js';
 import toolColors from './../stateManagement/toolColors.js';
 
-const isEmptyObject = (obj) => Object.keys(obj).length === 0 && obj.constructor === Object;
-
-/** Calculates the minimum, maximum, and mean value in the given pixel array */
-const calculateMinMaxMean = (storedPixelLuminanceData, globalMin, globalMax) => {
-  const numPixels = storedPixelLuminanceData.length;
-  let min = globalMax;
-  let max = globalMin;
-  let sum = 0;
-
-  if (numPixels < 2) {
-    return {
-      min,
-      max,
-      mean: (globalMin + globalMax) / 2
-    };
-  }
-
-  for (let index = 0; index < numPixels; index++) {
-    const spv = storedPixelLuminanceData[index];
-
-    min = Math.min(min, spv);
-    max = Math.max(max, spv);
-    sum += spv;
-  }
-
-  return {
-    min,
-    max,
-    mean: sum / numPixels
-  };
-};
-
-/** Calculates the minimum and maximum value in the given pixel array */
-const applyWWWCRegion = (evt, config) => {
-  const eventData = evt.detail;
-  const { image, element } = eventData;
-  const { start: startPoint, end: endPoint } = evt.detail.handles;
-
-  // Get the rectangular region defined by the handles
-  let left = Math.min(startPoint.x, endPoint.x);
-  let top = Math.min(startPoint.y, endPoint.y);
-  let width = Math.abs(startPoint.x - endPoint.x);
-  let height = Math.abs(startPoint.y - endPoint.y);
-
-  // Bound the rectangle so we don't get undefined pixels
-  left = clip(left, 0, image.width);
-  top = clip(top, 0, image.height);
-  width = Math.floor(Math.min(width, Math.abs(image.width - left)));
-  height = Math.floor(Math.min(height, Math.abs(image.height - top)));
-
-  // Get the pixel data in the rectangular region
-  const pixelLuminanceData = getLuminance(element, left, top, width, height);
-
-  // Calculate the minimum and maximum pixel values
-  const minMaxMean = calculateMinMaxMean(pixelLuminanceData, image.minPixelValue, image.maxPixelValue);
-
-  // Adjust the viewport window width and center based on the calculated values
-  const viewport = eventData.viewport;
-
-  if (config.minWindowWidth === undefined) {
-    config.minWindowWidth = 10;
-  }
-
-  viewport.voi.windowWidth = Math.max(Math.abs(minMaxMean.max - minMaxMean.min), config.minWindowWidth);
-  viewport.voi.windowCenter = minMaxMean.mean;
-
-  external.cornerstone.setViewport(element, viewport);
-  external.cornerstone.updateImage(element);
-};
-
 export default class extends baseTool {
   constructor (name) {
-    const strategies = {
-      applyWWWCRegion
-    };
-
     super({
       name: name || 'wwwcRegion',
-      strategies,
       supportedInteractionTypes: ['mouse', 'touch'],
       configuration: {
         minWindowWidth: 10
@@ -107,7 +32,9 @@ export default class extends baseTool {
     const context = getNewContext(eventData.canvasContext.canvas);
 
     draw(context, (context) => {
-      drawRect(context, element, this.handles.start, this.handles.end, { color });
+      drawRect(context, element, this.handles.start, this.handles.end, {
+        color
+      });
     });
   }
 
@@ -117,7 +44,7 @@ export default class extends baseTool {
 
   touchDragCallback (evt) {
     // Prevent CornerstoneToolsTouchStartActive from killing any press events
-    evt.stopImmediatePropagation();
+    // Evt.stopImmediatePropagation();
     this._setHandlesAndUpdate(evt);
   }
 
@@ -156,10 +83,10 @@ export default class extends baseTool {
   }
 
   /**
-  * Event handler for MOUSE_UP during handle drag event loop.
-  *
-  * @param {Object} evt - The event.
-  */
+   * Event handler for MOUSE_UP during handle drag event loop.
+   *
+   * @param {Object} evt - The event.
+   */
   _mouseUpCallback (evt) {
     if (isEmptyObject(this.handles.start) || isEmptyObject(this.handles.end)) {
       return;
@@ -169,7 +96,7 @@ export default class extends baseTool {
     const element = eventData.element;
 
     eventData.handles = this.handles;
-    this.applyActiveStrategy(evt);
+    applyWWWCRegion(evt, this.configuration);
     this._resetHandles();
     this._deactivateModify(element);
   }
@@ -182,12 +109,12 @@ export default class extends baseTool {
   }
 
   /**
-  * Adds modify loop event listeners.
-  *
-  * @private
-  * @param {Object} element - The viewport element to add event listeners to.
-  * @modifies {element}
-  */
+   * Adds modify loop event listeners.
+   *
+   * @private
+   * @param {Object} element - The viewport element to add event listeners to.
+   * @modifies {element}
+   */
   _activateModify (element) {
     element.addEventListener(EVENTS.MOUSE_UP, this._mouseUpCallback);
     element.addEventListener(EVENTS.MOUSE_CLICK, this._mouseUpCallback);
@@ -195,15 +122,114 @@ export default class extends baseTool {
   }
 
   /**
-  * Removes modify loop event listeners.
-  *
-  * @private
-  * @param {Object} element - The viewport element to add event listeners to.
-  * @modifies {element}
-  */
+   * Removes modify loop event listeners.
+   *
+   * @private
+   * @param {Object} element - The viewport element to add event listeners to.
+   * @modifies {element}
+   */
   _deactivateModify (element) {
     element.removeEventListener(EVENTS.MOUSE_UP, this._mouseUpCallback);
     element.removeEventListener(EVENTS.MOUSE_CLICK, this._mouseUpCallback);
     element.removeEventListener(EVENTS.MOUSE_MOVE, this._mouseMoveCallback);
   }
 }
+
+/**
+ *
+ *
+ * @param {*} obj
+ */
+const isEmptyObject = (obj) =>
+  Object.keys(obj).length === 0 && obj.constructor === Object;
+
+/**
+ * Calculates the minimum and maximum value in the given pixel array
+ *
+ * @param {*} evt
+ * @param {*} config
+ */
+const applyWWWCRegion = (evt, config) => {
+  const eventData = evt.detail;
+  const { image, element } = eventData;
+  const { start: startPoint, end: endPoint } = evt.detail.handles;
+
+  // Get the rectangular region defined by the handles
+  let left = Math.min(startPoint.x, endPoint.x);
+  let top = Math.min(startPoint.y, endPoint.y);
+  let width = Math.abs(startPoint.x - endPoint.x);
+  let height = Math.abs(startPoint.y - endPoint.y);
+
+  // Bound the rectangle so we don't get undefined pixels
+  left = clip(left, 0, image.width);
+  top = clip(top, 0, image.height);
+  width = Math.floor(Math.min(width, Math.abs(image.width - left)));
+  height = Math.floor(Math.min(height, Math.abs(image.height - top)));
+
+  // Get the pixel data in the rectangular region
+  const pixelLuminanceData = getLuminance(element, left, top, width, height);
+
+  // Calculate the minimum and maximum pixel values
+  const minMaxMean = calculateMinMaxMean(
+    pixelLuminanceData,
+    image.minPixelValue,
+    image.maxPixelValue
+  );
+
+  // Adjust the viewport window width and center based on the calculated values
+  const viewport = eventData.viewport;
+
+  if (config.minWindowWidth === undefined) {
+    config.minWindowWidth = 10;
+  }
+
+  viewport.voi.windowWidth = Math.max(
+    Math.abs(minMaxMean.max - minMaxMean.min),
+    config.minWindowWidth
+  );
+  viewport.voi.windowCenter = minMaxMean.mean;
+
+  external.cornerstone.setViewport(element, viewport);
+  external.cornerstone.updateImage(element);
+};
+
+/**
+ * Calculates the minimum, maximum, and mean value in the given pixel array
+ *
+ * @param {*} storedPixelLuminanceData
+ * @param {*} globalMin
+ * @param {*} globalMax
+ * @returns
+ */
+const calculateMinMaxMean = (
+  storedPixelLuminanceData,
+  globalMin,
+  globalMax
+) => {
+  const numPixels = storedPixelLuminanceData.length;
+  let min = globalMax;
+  let max = globalMin;
+  let sum = 0;
+
+  if (numPixels < 2) {
+    return {
+      min,
+      max,
+      mean: (globalMin + globalMax) / 2
+    };
+  }
+
+  for (let index = 0; index < numPixels; index++) {
+    const spv = storedPixelLuminanceData[index];
+
+    min = Math.min(min, spv);
+    max = Math.max(max, spv);
+    sum += spv;
+  }
+
+  return {
+    min,
+    max,
+    mean: sum / numPixels
+  };
+};
