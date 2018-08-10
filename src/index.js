b2486ec146807e853852f1e34faf9982fec8c258
Gustavo Lelis <galelis@gmail.com>
2018-08-10 17:20:37 -0300

    Migrating all functions and start example changes

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ export { default as wwwcRegionTool } from './tools/wwwcRegionTool.js';
 export { default as zoomTool } from './tools/zoomTool.js';
 export { default as zoomTouchPinchTool } from './tools/zoomTouchPinchTool.js';
 export { default as zoomMouseWheelTool } from './tools/zoomMouseWheelTool.js';
-export { default as crosshairTool } from './tools/crosshairTool.js';
+export { default as crosshairsTool } from './tools/crosshairsTool.js';
+export { crosshairs } from './tools/crosshairs.js';
 
 // END V3 EXPORTS
 
