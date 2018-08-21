ec556a57953ff401283340f89c87174c267d7165
Gustavo Lelis <galelis@gmail.com>
2018-08-21 17:24:13 -0300

    RotateTouch Tool migration to cornerstoneTools v3

---
+++ src/tools/RotateTouchTool.js
@@ -0,0 +1,21 @@
+/* eslint class-methods-use-this: 0 */
+import external from './../externalModules.js';
+import baseTool from './../base/BaseTool.js';
+
+export default class extends baseTool {
+  constructor (name = 'RotateTouch') {
+    super({
+      name,
+      supportedInteractionTypes: ['touch']
+    });
+  }
+
+  touchRotateCallback (evt) {
+    const eventData = evt.detail;
+    const { element, viewport, rotation } = eventData;
+
+    viewport.rotation += rotation;
+    external.cornerstone.setViewport(element, viewport);
+  }
+}
+
