13906e51118d5d064b141c0586c6a38a50a20ad1
James Petts <jpetts@12136phydt.icr.ac.uk>
2018-08-30 13:50:48 +0100

    Fixing last test

--- src/fancy-tools/wwwcTool.test.js
+++ src/fancy-tools/wwwcTool.test.js
@@ -3,15 +3,15 @@ import external from './../externalModules.js';
 
 jest.mock('./../externalModules.js', () => ({
   cornerstone: {
-    setViewport: jest.fn()
-  },
-  colors: {
-    getColormap: jest.fn().mockImplementation(() => {
-      return {
-        setNumberOfColors: jest.fn(),
-        setColor: jest.fn()
-      }
-    })
+    setViewport: jest.fn(),
+    colors: {
+      getColormap: jest.fn().mockImplementation(() => {
+        return {
+          setNumberOfColors: jest.fn(),
+          setColor: jest.fn()
+        }
+      })
+    }
   }
 }));
 
