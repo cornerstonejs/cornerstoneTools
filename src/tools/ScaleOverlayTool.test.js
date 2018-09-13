6abe314e9c26bc07e7ab3140ec596fafec836800
James Petts <jpetts@12136phydt.icr.ac.uk>
2018-09-13 14:01:08 +0100

    Fixed up broken unit tests

--- src/tools/ScaleOverlayTool.test.js
+++ src/tools/ScaleOverlayTool.test.js
@@ -118,7 +118,7 @@ describe('ScaleOverlayTool.js', () => {
       instantiatedTool.renderToolData(mockEvt);
 
       expect(console.warn).toHaveBeenCalled();
-      expect(console.warn.mock.calls[0][0]).toContain('unable to define rowPixelSpacing or colPixelSpacing from data on scaleOverlay\'s renderToolData');
+      expect(console.warn.mock.calls[0][0]).toContain('unable to define rowPixelSpacing or colPixelSpacing from data on ScaleOverlay\'s renderToolData');
     });
   });
 });
