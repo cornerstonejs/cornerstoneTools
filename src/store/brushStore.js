803e513a0e56ce0c9863d6ce9c29f298c587c9b9
James Petts <jpetts@12136phydt.icr.ac.uk>
2018-09-17 10:32:26 +0100

    Added bracket that got nuked from merge

--- src/store/brushStore.js
+++ src/store/brushStore.js
@@ -83,23 +83,24 @@ export default {
 if (external.cornerstone && external.cornerstone.colors) {
   const colormap = external.cornerstone.colors.getColormap(state.colorMapId);
 
-colormap.setNumberOfColors(19);
-colormap.setColor(0, [230, 25, 75, 255]);
-colormap.setColor(1, [60, 180, 175, 255]);
-colormap.setColor(2, [255, 225, 25, 255]);
-colormap.setColor(3, [0, 130, 200, 255]);
-colormap.setColor(4, [245, 130, 48, 255]);
-colormap.setColor(5, [145, 30, 180, 255]);
-colormap.setColor(6, [70, 240, 240, 255]);
-colormap.setColor(7, [240, 50, 230, 255]);
-colormap.setColor(8, [210, 245, 60, 255]);
-colormap.setColor(9, [250, 190, 190, 255]);
-colormap.setColor(10, [0, 128, 128, 255]);
-colormap.setColor(11, [230, 190, 255, 255]);
-colormap.setColor(12, [170, 110, 40, 255]);
-colormap.setColor(13, [255, 250, 200, 255]);
-colormap.setColor(14, [128, 0, 0, 255]);
-colormap.setColor(15, [170, 255, 195, 255]);
-colormap.setColor(16, [128, 128, 0, 255]);
-colormap.setColor(17, [255, 215, 180, 255]);
-colormap.setColor(18, [0, 0, 128, 255]);
+  colormap.setNumberOfColors(19);
+  colormap.setColor(0, [230, 25, 75, 255]);
+  colormap.setColor(1, [60, 180, 175, 255]);
+  colormap.setColor(2, [255, 225, 25, 255]);
+  colormap.setColor(3, [0, 130, 200, 255]);
+  colormap.setColor(4, [245, 130, 48, 255]);
+  colormap.setColor(5, [145, 30, 180, 255]);
+  colormap.setColor(6, [70, 240, 240, 255]);
+  colormap.setColor(7, [240, 50, 230, 255]);
+  colormap.setColor(8, [210, 245, 60, 255]);
+  colormap.setColor(9, [250, 190, 190, 255]);
+  colormap.setColor(10, [0, 128, 128, 255]);
+  colormap.setColor(11, [230, 190, 255, 255]);
+  colormap.setColor(12, [170, 110, 40, 255]);
+  colormap.setColor(13, [255, 250, 200, 255]);
+  colormap.setColor(14, [128, 0, 0, 255]);
+  colormap.setColor(15, [170, 255, 195, 255]);
+  colormap.setColor(16, [128, 128, 0, 255]);
+  colormap.setColor(17, [255, 215, 180, 255]);
+  colormap.setColor(18, [0, 0, 128, 255]);
+}
