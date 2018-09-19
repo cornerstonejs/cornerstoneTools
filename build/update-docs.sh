45d2fcee178e8a9bb8da2951cfeeb05c9dd96d07
dannyrb <danny.ri.brown@gmail.com>
2018-09-18 21:05:50 -0400

    Try to merge multiple versions of docs

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -2,13 +2,13 @@ cd docs
 rm -rf _book
 gitbook install
 gitbook build
+git clone -b gh-pages --single-branch git@github.com:cornerstonejs/cornerstoneTools.git _live-book/
 cd _book/
 mkdir v3
 mv -v * v3/
 cd ..
 cp assets/CNAME _book/CNAME
 cd _book
-git clone git@github.com:cornerstonejs/cornerstoneTools.git master:gh-pages
 git add -A
 git commit -m 'update book'
 git push -f git@github.com:cornerstonejs/cornerstoneTools.git master:gh-pages
