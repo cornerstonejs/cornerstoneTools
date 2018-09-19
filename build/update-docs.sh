6a94b1a9c69ed6043e352dde3190b8f2d306c66a
dannyrb <danny.ri.brown@gmail.com>
2018-09-18 21:42:08 -0400

    Next iteration

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -1,29 +1,29 @@
-# # Generate this version's "book"
-# cd docs
-# rm -rf _book
-# gitbook install
-# gitbook build
+# Generate this version's "book"
+cd docs
+rm -rf _book
+gitbook install
+gitbook build
 
-# # Move our book to it's own version folder
-# cd _book/
-# mkdir v3.0.0
-# mv -v * v3.0.0/
-# cd ..
+# Move our book to it's own version folder
+cd _book/
+mkdir v3.0.0
+mv -v * v3.0.0/
+cd ..
 
-# # Grab our current gh-pages files
-# # Remove it's version 3, and move it to our _book
-# git clone -b gh-pages --single-branch git@github.com:cornerstonejs/cornerstoneTools.git _live-book/
-# rm -rf _live-book/v3.0.0/
-# mv -v _live-book/* _book/
+# Grab our current gh-pages files
+# Remove it's version 3, and move it to our _book
+git clone -b gh-pages --single-branch git@github.com:cornerstonejs/cornerstoneTools.git _live-book/
+rm -rf _live-book/v3.0.0/
+mv -v _live-book/* _book/
 
-# # Commit & Push
+# Commit & Push
 cd _book/
 git init
 git add -A
 git commit -m 'update book'
-# git push -f git@github.com:cornerstonejs/cornerstoneTools.git master:gh-pages
-# cd ..
+git push -f git@github.com:cornerstonejs/cornerstoneTools.git master:gh-pages
+cd ..
 
-# # Cleanup
-# rm -rf _live-book
-# rm -rf _book
+# Cleanup
+rm -rf _live-book
+rm -rf _book
