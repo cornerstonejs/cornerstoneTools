d43d77c0470907171ed6a2b0bc2d10aafcd763e4
dannyrb <danny.ri.brown@gmail.com>
2018-09-22 12:27:41 -0400

    Shift .bat up a directory so it's not deployed to gh-pages (breaks deploy)

--- docs/v2/win-server-workaround.bat
+++
@@ -1,8 +0,0 @@
-:: `gitbook serve` can break with permission issues on Windows for misc.
-:: machines. This script is a workaround. When `gitbook serve` fails, it silently
-:: restarts it.
-:: https://github.com/GitbookIO/gitbook/issues/1379#issuecomment-288048275
-@Echo off
-:Start
-call gitbook serve
-goto Start
--- docs/win-server-workaround.bat
+++ docs/win-server-workaround.bat
@@ -1,3 +1,6 @@
+:: Call this script from the command line while in the directory of the book
+:: you want to actively edit + preview
+::
 :: `gitbook serve` can break with permission issues on Windows for misc.
 :: machines. This script is a workaround. When `gitbook serve` fails, it silently
 :: restarts it.
