dad80627737ecc788e2157fa431ab5f9e4d7ee5c
James Petts <jpetts@12136phydt.icr.ac.uk>
2018-10-08 16:01:06 +0100

    Pulled in new drawing API updates.

--- src/util/drawing.js
+++ src/util/drawing.js
@@ -1,6 +1,5 @@
 import external from '../externalModules.js';
-import toolStyle from '../stateManagement/toolStyle.js';
-import textStyle from '../stateManagement/textStyle.js';
+import { toolStyle, textStyle } from '../index.js';
 
 /**
  * A {@link https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillStyle|color, gradient or pattern} to use inside shapes.
@@ -278,6 +277,38 @@ export function drawRect (context, element, corner1, corner2, options, coordSyst
     context.rect(left, top, width, height);
   });
 }
+
+/**
+ * Fill the region outside a rectangle defined by `corner1` and `corner2`.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {HTMLElement} element - The DOM Element to draw on
+ * @param {Object} corner1 - `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} corner2 - `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} options - See {@link path}
+ * @param {String} [coordSystem='pixel'] - Can be "pixel" (default) or "canvas". The coordinate
+ *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
+ *     is used to transform the points from pixel to canvas coordinates.
+ */
+export function fillOutsideRect (context, element, corner1, corner2, options, coordSystem = 'pixel') {
+  if (coordSystem === 'pixel') {
+    const cornerstone = external.cornerstone;
+
+    corner1 = cornerstone.pixelToCanvas(element, corner1);
+    corner2 = cornerstone.pixelToCanvas(element, corner2);
+  }
+
+  const left = Math.min(corner1.x, corner2.x);
+  const top = Math.min(corner1.y, corner2.y);
+  const width = Math.abs(corner1.x - corner2.x);
+  const height = Math.abs(corner1.y - corner2.y);
+
+  path(context, options, (context) => {
+    context.rect(0, 0, context.canvas.clientWidth, context.canvas.clientHeight);
+    context.rect(left + width, top, -width, height);
+  });
+}
+
 
 /**
  * Draw a filled rectangle defined by `boundingBox` using the style defined by `fillStyle`.
