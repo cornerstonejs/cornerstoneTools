d28becc3f8e749e035ff25469787ade5fe3e0478
dannyrb <danny.ri.brown@gmail.com>
2018-10-23 22:24:51 -0400

    Ability to override default tool configuration when adding the tool to an enabled element

--- src/store/addTool.js
+++ src/store/addTool.js
@@ -2,17 +2,19 @@ import { state } from './index.js';
 import getToolForElement from './getToolForElement.js';
 
 /**
- * Adds a tool to an element.
+ * Adds a tool to an enabled element.
+ *
  * @export @public @method
  * @name addToolForElement
  * @param {HTMLElement} element The element to add the tool to.
  * @param {baseTool} tool The tool to add to the element.
+ * @param {configuration} [configuration] Override the default tool configuration
  */
-const addToolForElement = function (element, apiTool) {
+const addToolForElement = function (element, apiTool, configuration) {
   // Instantiating the tool here makes it harder to accidentally add
   // The same tool (by reference) for multiple elements (which would reassign the tool
   // To a new element).
-  const tool = new apiTool();
+  const tool = new apiTool(configuration);
 
   const toolAlreadyAddedToElement = getToolForElement(element, tool.name);
 
