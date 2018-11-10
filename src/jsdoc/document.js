10d795480004225d6be29c5878c3632ed4951668
dannyrb <danny.ri.brown@gmail.com>
2018-11-10 14:29:31 -0500

    Add namespace for native DOM events

---
+++ src/jsdoc/document.js
@@ -0,0 +1,30 @@
+/**
+ *  Namespace for native DOM/Document types
+ * @namespace document
+ */
+
+/**
+ * Native `mousedown` event
+ *
+ * @event document#mousedown
+ * @type {Object}
+ * @property {HTMLElement} target
+ * @property {number} which
+ * @property {number} pageX
+ * @property {number} pageY
+ * @property {number} clientX
+ * @property {number} clientY
+ */
+
+/**
+ * Native `mouseup` event
+ *
+ * @event document#mouseup
+ * @type {Object}
+ * @property {HTMLElement} target
+ * @property {number} which
+ * @property {number} pageX
+ * @property {number} pageY
+ * @property {number} clientX
+ * @property {number} clientY
+ */
