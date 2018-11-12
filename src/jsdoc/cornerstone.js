9962ccc7d92e4d12eb8c31409d89fc606607f94a
dannyrb <danny.ri.brown@gmail.com>
2018-11-12 09:44:22 -0500

    Add a namespace for cornerstone events

---
+++ src/jsdoc/cornerstone.js
@@ -0,0 +1,19 @@
+/**
+ * Namespace for Cornerstone events
+ * @namespace Cornerstone
+ */
+
+/**
+ * Cornerstone's `IMAGE_RENDERED` event
+ *
+ * @event cornerstoneimagerendered
+ * @type {Object}
+ * @memberof Cornerstone
+ *
+ * @property {HTMLElement} target
+ * @property {number} which
+ * @property {number} pageX
+ * @property {number} pageY
+ * @property {number} clientX
+ * @property {number} clientY
+ */
