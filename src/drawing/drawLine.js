cbfa8e6f97b0d92b8cf448d24fad2da1d79c2fb7
dannyrb <danny.ri.brown@gmail.com>
2018-11-12 15:59:48 -0500

    Fix drawLine method

--- src/drawing/drawLine.js
+++ src/drawing/drawLine.js
@@ -1,42 +1,38 @@
 import path from './path.js';
 
 /**
- * Draw multiple lines.
+ * Draw a line between `start` and `end`.
+ *
  * @public
- * @method drawLines
+ * @method drawLine
  * @memberof CornerstoneTools.Drawing
  *
- * @param {CanvasRenderingContext2D} context - Target context
+ * @param {CanvasRenderingContext2D} context
  * @param {HTMLElement} element - The DOM Element to draw on
- * @param {Object[]} lines - `[{ start: {x, y}, end: { x, y }]` An array of `start`, `end` pairs.
- *     Each point is `{ x, y }` in either pixel or canvas coordinates.
+ * @param {Object} start - `{ x, y } in either pixel or canvas coordinates.
+ * @param {Object} end - `{ x, y }` in either pixel or canvas coordinates.
  * @param {Object} options - See {@link path}
  * @param {String} [coordSystem='pixel'] - Can be "pixel" (default) or "canvas". The coordinate
  *     system of the points passed in to the function. If "pixel" then cornerstone.pixelToCanvas
  *     is used to transform the points from pixel to canvas coordinates.
- * @returns {undefined}
  */
-export default function(
+export function drawLine(
   context,
   element,
-  lines,
+  start,
+  end,
   options,
   coordSystem = 'pixel'
 ) {
   path(context, options, context => {
-    lines.forEach(line => {
-      let start = line.start;
-      let end = line.end;
+    if (coordSystem === 'pixel') {
+      const cornerstone = external.cornerstone;
 
-      if (coordSystem === 'pixel') {
-        const cornerstone = external.cornerstone;
+      start = cornerstone.pixelToCanvas(element, start);
+      end = cornerstone.pixelToCanvas(element, end);
+    }
 
-        start = cornerstone.pixelToCanvas(element, start);
-        end = cornerstone.pixelToCanvas(element, end);
-      }
-
-      context.moveTo(start.x, start.y);
-      context.lineTo(end.x, end.y);
-    });
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
   });
 }
