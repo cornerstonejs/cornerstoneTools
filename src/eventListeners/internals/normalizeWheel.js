338ad169cb89c4fe68f114429cc6d5b0408dbead
dannyrb <danny.ri.brown@gmail.com>
2018-11-26 22:08:00 -0500

    Add spin and pixel properties to cornerstonetoolsmousewheel event

--- src/eventListeners/internals/normalizeWheel.js
+++ src/eventListeners/internals/normalizeWheel.js
@@ -1,119 +1,62 @@
-const useHasFeature =
-  document.implementation &&
-  document.implementation.hasFeature &&
-  document.implementation.hasFeature('', '') !== true;
-
 // Reasonable defaults
 const PIXEL_STEP = 10;
 const LINE_HEIGHT = 40;
 const PAGE_HEIGHT = 800;
 
-/**
- * As of today, there are 2 DOM event types you can listen to:
- *
- *   'wheel'                -- Chrome(31+), FF(17+), IE(9+)
- *   'mousewheel'           -- Chrome, IE(6+), Opera, Safari
- *
- * So what to do?  The is the best:
- *
- *   normalizeWheel.getEventType();
- */
-const normalizeWheel = function(event) {
-  let sX = 0,
-    sY = 0, // SpinX, spinY
-    pX = 0,
-    pY = 0; // PixelX, pixelY
+export default function(event) {
+  let spinX = 0,
+    spinY = 0,
+    pixelX = 0,
+    pixelY = 0;
 
   // Legacy
   if ('detail' in event) {
-    sY = event.detail;
+    spinY = event.detail;
   }
   if ('wheelDelta' in event) {
-    sY = -event.wheelDelta / 120;
+    spinY = -event.wheelDelta / 120;
   }
   if ('wheelDeltaY' in event) {
-    sY = -event.wheelDeltaY / 120;
+    spinY = -event.wheelDeltaY / 120;
   }
   if ('wheelDeltaX' in event) {
-    sX = -event.wheelDeltaX / 120;
+    spinX = -event.wheelDeltaX / 120;
   }
 
-  // Side scrolling on FF with DOMMouseScroll
-  if ('axis' in event && event.axis === event.HORIZONTAL_AXIS) {
-    sX = sY;
-    sY = 0;
-  }
-
-  pX = sX * PIXEL_STEP;
-  pY = sY * PIXEL_STEP;
+  pixelX = spinX * PIXEL_STEP;
+  pixelY = spinY * PIXEL_STEP;
 
   if ('deltaY' in event) {
-    pY = event.deltaY;
+    pixelY = event.deltaY;
   }
   if ('deltaX' in event) {
-    pX = event.deltaX;
+    pixelX = event.deltaX;
   }
 
-  if ((pX || pY) && event.deltaMode) {
+  if ((pixelX || pixelY) && event.deltaMode) {
     if (event.deltaMode === 1) {
       // Delta in LINE units
-      pX *= LINE_HEIGHT;
-      pY *= LINE_HEIGHT;
+      pixelX *= LINE_HEIGHT;
+      pixelY *= LINE_HEIGHT;
     } else {
       // Delta in PAGE units
-      pX *= PAGE_HEIGHT;
-      pY *= PAGE_HEIGHT;
+      pixelX *= PAGE_HEIGHT;
+      pixelY *= PAGE_HEIGHT;
     }
   }
 
   // Fall-back if spin cannot be determined
-  if (pX && !sX) {
-    sX = pX < 1 ? -1 : 1;
+  if (pixelX && !spinX) {
+    spinX = pixelX < 1 ? -1 : 1;
   }
-  if (pY && !sY) {
-    sY = pY < 1 ? -1 : 1;
+  if (pixelY && !spinY) {
+    spinY = pixelY < 1 ? -1 : 1;
   }
 
   return {
-    spinX: sX,
-    spinY: sY,
-    pixelX: pX,
-    pixelY: pY,
+    spinX,
+    spinY,
+    pixelX,
+    pixelY,
   };
-};
-
-const getEventType = function() {
-  return _isWheelEventSupported() ? 'wheel' : 'mousewheel';
-};
-
-export default {
-  normalizeWheel,
-  getEventType,
-};
-
-/**
- * Checks if the weheel event is supported
- *
- * @private
- * @returns {boolean} True if the event is supported.
- * @license Modernizr 3.0.0pre (Custom Build) | MIT
- */
-function _isWheelEventSupported() {
-  const eventName = 'onwheel';
-  let isSupported = eventName in document;
-
-  // 2nd Check
-  if (!isSupported) {
-    const element = document.createElement('div');
-
-    element.setAttribute(eventName, 'return;');
-    isSupported = typeof element[eventName] === 'function';
-  }
-
-  // 3rd check
-  if (!isSupported && useHasFeature) {
-    isSupported = document.implementation.hasFeature('Events.wheel', '3.0');
-  }
-
-  return isSupported;
 }
--- src/eventListeners/mouseWheelEventListeners.js
+++ src/eventListeners/mouseWheelEventListeners.js
@@ -1,75 +1,45 @@
 import EVENTS from '../events.js';
 import external from '../externalModules.js';
 import triggerEvent from '../util/triggerEvent.js';
-import nm from './internals/normalizeWheel.js';
+import normalizeWheel from './internals/normalizeWheel.js';
 
-function mouseWheel(e) {
-  const element = e.currentTarget;
+/**
+ *
+ *
+ * @param {WheelEvent} evt
+ * @returns {undefined}
+ */
+function mouseWheel(evt) {
+  const element = evt.currentTarget;
   const enabledElement = external.cornerstone.getEnabledElement(element);
-
-  console.debug(`mouseWheel`);
-  console.log(e);
-  const NORMALIZED = nm.normalizeWheel(e);
-
-  console.debug(NORMALIZED);
 
   if (!enabledElement.image) {
     return;
   }
 
-  // !!!HACK/NOTE/WARNING!!!
-  // For some reason I am getting mousewheel and DOMMouseScroll events on my
-  // Mac os x mavericks system when middle mouse button dragging.
-  // I couldn't find any info about this so this might break other systems
-  // Webkit hack
-  // If (e.type === 'mousewheel' && e.wheelDeltaY === 0) {
-  //   Return;
-  // }
-  // // Firefox hack
-  // If (e.type === 'DOMMouseScroll' && e.axis === 1) {
-  //   Return;
-  // }
+  evt.preventDefault();
 
-  e.preventDefault();
-
-  let x;
-  let y;
-
-  if (e.pageX !== undefined && e.pageY !== undefined) {
-    x = e.pageX;
-    y = e.pageY;
-  } else {
-    // IE9 & IE10
-    x = e.x;
-    y = e.y;
-  }
-
-  const startingCoords = external.cornerstone.pageToPixel(element, x, y);
-
-  e = window.event && window.event.wheelDelta ? window.event : e; // Old IE support
-
-  let wheelDelta;
-
-  if (e.wheelDelta) {
-    wheelDelta = e.wheelDelta;
-  } else if (e.deltaY) {
-    wheelDelta = -e.deltaY;
-  } else if (e.detail) {
-    wheelDelta = -e.detail;
-  } else {
-    wheelDelta = e.wheelDelta;
-  }
-
-  const direction = wheelDelta < 0 ? -1 : 1;
+  const { pageX, pageY } = evt;
+  const startingCoords = external.cornerstone.pageToPixel(
+    element,
+    pageX,
+    pageY
+  );
+  const { spinX, spinY, pixelX, pixelY } = normalizeWheel(evt);
+  const direction = spinY < 0 ? -1 : 1;
 
   const mouseWheelData = {
     element,
     viewport: external.cornerstone.getViewport(element),
-    detail: e,
+    detail: evt,
     image: enabledElement.image,
     direction,
-    pageX: x,
-    pageY: y,
+    spinX,
+    spinY,
+    pixelX,
+    pixelY,
+    pageX,
+    pageY,
     imageX: startingCoords.x,
     imageY: startingCoords.y,
   };
