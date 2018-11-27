2b09ecf1386b3a8ae567921ccd2c9cbda9ead1f6
dannyrb <danny.ri.brown@gmail.com>
2018-11-27 10:44:13 -0500

    Add wheelEventListener tests

--- src/eventListeners/wheelEventListener.js
+++ src/eventListeners/wheelEventListener.js
@@ -1,3 +1,8 @@
+/**
+ * Internal module used to turn on listening, handling, and normalizing of the
+ * native `wheel` event
+ */
+
 import EVENTS from '../events.js';
 import external from '../externalModules.js';
 import triggerEvent from '../util/triggerEvent.js';
@@ -5,11 +10,12 @@ import normalizeWheel from './internals/normalizeWheel.js';
 
 /**
  *
- *
+ * @private
+ * @function wheelEventHandler
  * @param {WheelEvent} evt
  * @returns {undefined}
  */
-function mouseWheel(evt) {
+function wheelEventHandler(evt) {
   const element = evt.currentTarget;
   const enabledElement = external.cornerstone.getEnabledElement(element);
 
@@ -47,13 +53,29 @@ function mouseWheel(evt) {
   triggerEvent(element, EVENTS.MOUSE_WHEEL, mouseWheelData);
 }
 
+/**
+ * Listens for the wheel event, and handles it. Handled event
+ * will be "normalized" and re-emitted as `EVENTS.MOUSE_WHEEL`
+ *
+ * @private
+ * @param {HTMLElement} element
+ * @returns {undefined}
+ */
 function enable(element) {
   disable(element);
-  element.addEventListener('wheel', mouseWheel, { passive: false });
+  element.addEventListener('wheel', wheelEventHandler, { passive: false });
 }
 
+/**
+ * Removes listener and handler for wheel event. `EVENTS.MOUSE_WHEEL`
+ * will no longer be emitted.
+ *
+ * @private
+ * @param {HTMLElement} element
+ * @returns {undefined}
+ */
 function disable(element) {
-  element.removeEventListener('wheel', mouseWheel, { passive: false });
+  element.removeEventListener('wheel', wheelEventHandler, { passive: false });
 }
 
 export default {
