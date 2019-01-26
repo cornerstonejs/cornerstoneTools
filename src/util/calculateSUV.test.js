336663a4dc82f5b5db4ed2dd60c8ac740e5bd158
dannyrb <danny.ri.brown@gmail.com>
2019-01-25 23:14:19 -0500

    Play w/ test re-order

--- src/util/calculateSUV.test.js
+++ src/util/calculateSUV.test.js
@@ -5,32 +5,7 @@ import calculateSUV from './calculateSUV.js';
 jest.mock('./../externalModules.js', () => ({
   cornerstone: {
     metaData: {
-      get: jest
-        .fn()
-        .mockReturnValueOnce({
-          patientWeight: 1,
-        })
-        .mockReturnValueOnce({
-          modality: 'PT',
-          seriesTime: {
-            fractionalSeconds: 0,
-            seconds: 0,
-            minutes: 0,
-            hours: 0,
-          },
-        })
-        .mockReturnValueOnce({
-          radiopharmaceuticalInfo: {
-            radiopharmaceuticalStartTime: {
-              fractionalSeconds: 0,
-              seconds: 0,
-              minutes: 0,
-              hours: 0,
-            },
-            radionuclideTotalDose: 1,
-            radionuclideHalfLife: 1,
-          },
-        }),
+      get: jest.fn(),
     },
   },
 }));
@@ -41,27 +16,65 @@ const image = {
   intercept: 1,
 };
 
+const patientStub = {
+  patientWeight: 1,
+};
+
+const generalStub = {
+  modality: 'PT',
+  seriesTime: {
+    fractionalSeconds: 0,
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+  },
+};
+const petStub = {
+  radiopharmaceuticalInfo: {
+    radiopharmaceuticalStartTime: {
+      fractionalSeconds: 0,
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+    },
+    radionuclideTotalDose: 1,
+    radionuclideHalfLife: 1,
+  },
+};
+
 describe('calculateSUV.js', () => {
   it('calculates SUV by scaling the pixel value by default', () => {
     // Setup
     const pixelValue = 10;
 
+    external.cornerstone.metaData.get = jest
+      .fn()
+      .mockReturnValueOnce(patientStub)
+      .mockReturnValueOnce(generalStub)
+      .mockReturnValueOnce(petStub);
+
     //
     const result = calculateSUV(image, pixelValue);
 
     // Assert
-    expect(result).toBe(1100);
+    expect(result).toBe(11000);
   });
-});
 
-it('does not rescale the pixel value when skipRescale is true', () => {
-  // Setup
-  const pixelValue = 10;
-  const skipRescale = true;
+  it('does not rescale the pixel value when skipRescale is true', () => {
+    // Setup
+    const pixelValue = 10;
+    const skipRescale = true;
 
-  //
-  const result = calculateSUV(image, pixelValue, skipRescale);
+    external.cornerstone.metaData.get = jest
+      .fn()
+      .mockReturnValueOnce(patientStub)
+      .mockReturnValueOnce(generalStub)
+      .mockReturnValueOnce(petStub);
 
-  // Assert
-  expect(result).toBe(1000);
+    //
+    const result = calculateSUV(image, pixelValue, skipRescale);
+
+    // Assert
+    expect(result).toBe(10000);
+  });
 });
