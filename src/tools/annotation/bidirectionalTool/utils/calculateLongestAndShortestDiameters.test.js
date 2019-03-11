87ca6a06464174d08654bdadca9fbc0d96a5c5f0
Gustavo Lelis <galelis@gmail.com>
2019-03-11 15:07:54 -0300

    fix(bidirectional): Fix unit tests after callouts

--- src/tools/annotation/bidirectionalTool/utils/calculateLongestAndShortestDiameters.test.js
+++ src/tools/annotation/bidirectionalTool/utils/calculateLongestAndShortestDiameters.test.js
@@ -1,26 +1,7 @@
 import calculateLongestAndShortestDiameters from './calculateLongestAndShortestDiameters.js';
-import lodash from 'lodash';
 
-const measurementData = {
-  handles: {
-    start: {
-      x: 290.6229508196721,
-      y: 272.7868852459016,
-    },
-    end: {
-      x: 361.96721311475403,
-      y: 191.99999999999997,
-    },
-    perpendicularStart: {
-      x: 346.49180327868845,
-      y: 250.22950819672127,
-    },
-    perpendicularEnd: {
-      x: 306.0983606557376,
-      y: 214.55737704918027,
-    },
-  },
-};
+let measurementData = {};
+let eventData = {};
 
 describe('CalculateLongestAndShortestDiameters.js', () => {
   beforeEach(() => {
@@ -28,92 +9,103 @@ describe('CalculateLongestAndShortestDiameters.js', () => {
     console.error.mockClear();
     console.warn = jest.fn();
     console.warn.mockClear();
-  });
 
-  describe('calculate diameters', () => {
-    it('once we pass all data needed', () => {
-      const newMeasurementData = lodash.cloneDeep(measurementData);
-      const eventData = {
-        image: {
-          rowPixelSpacing: 0.876953125,
-          columnPixelSpacing: 0.876953125,
-        },
-      };
-
-      calculateLongestAndShortestDiameters(eventData, newMeasurementData);
-
-      expect(newMeasurementData.longestDiameter).toEqual('94.5');
-      expect(newMeasurementData.shortestDiameter).toEqual('47.3');
-    });
-
-    it('once pixelSpacing is undefined', () => {
-      const newMeasurementData = lodash.cloneDeep(measurementData);
-      const eventData = {
-        image: {},
-      };
-
-      calculateLongestAndShortestDiameters(eventData, newMeasurementData);
-
-      expect(newMeasurementData.longestDiameter).toEqual('107.8');
-      expect(newMeasurementData.shortestDiameter).toEqual('53.9');
-    });
-
-    it('once shortestDiameter gets zero because perpendicular axis are undefined', () => {
-      const newMeasurementData = lodash.cloneDeep(measurementData);
-
-      newMeasurementData.handles = {
+    measurementData = {
+      handles: {
         start: {
-          x: 0,
-          y: 0,
+          x: 290.6229508196721,
+          y: 272.7868852459016,
         },
         end: {
-          x: 4,
-          y: 4,
-        },
-        perpendicularStart: {},
-        perpendicularEnd: {},
-      };
-
-      const eventData = {
-        image: {},
-      };
-
-      calculateLongestAndShortestDiameters(eventData, newMeasurementData);
-
-      expect(newMeasurementData.shortestDiameter).toEqual('0.0');
-      expect(newMeasurementData.longestDiameter).toEqual('5.7');
-    });
-
-    it('once shortestDiameter gets swaped with longestDiamater', () => {
-      const newMeasurementData = lodash.cloneDeep(measurementData);
-
-      newMeasurementData.handles = {
-        start: {
-          x: 10,
-          y: 10,
-        },
-        end: {
-          x: 5,
-          y: 5,
+          x: 361.96721311475403,
+          y: 191.99999999999997,
         },
         perpendicularStart: {
-          x: 12,
-          y: 12,
+          x: 346.49180327868845,
+          y: 250.22950819672127,
         },
         perpendicularEnd: {
-          x: 5,
-          y: 5,
+          x: 306.0983606557376,
+          y: 214.55737704918027,
         },
-      };
+      },
+    };
+    eventData = {
+      image: {},
+    };
+  });
 
-      const eventData = {
-        image: {},
-      };
+  it('should calculates long/short diameters', () => {
+    eventData = {
+      image: {
+        rowPixelSpacing: 0.876953125,
+        columnPixelSpacing: 0.876953125,
+      },
+    };
 
-      calculateLongestAndShortestDiameters(eventData, newMeasurementData);
+    calculateLongestAndShortestDiameters(eventData, measurementData);
 
-      expect(newMeasurementData.shortestDiameter).toEqual('7.1');
-      expect(newMeasurementData.longestDiameter).toEqual('9.9');
-    });
+    expect(measurementData.longestDiameter).toEqual('94.5');
+    expect(measurementData.shortestDiameter).toEqual('47.3');
+  });
+
+  it('should return values with scale of 1', () => {
+    calculateLongestAndShortestDiameters(eventData, measurementData);
+
+    expect(measurementData.shortestDiameter).toMatch(/\d*\.\d$/);
+    expect(measurementData.longestDiameter).toMatch(/\d*\.\d$/);
+  });
+
+  it('should use a default pixelSpacing of 1 when pixelSpacing is undefined', () => {
+    calculateLongestAndShortestDiameters(eventData, measurementData);
+
+    expect(measurementData.longestDiameter).toEqual('107.8');
+    expect(measurementData.shortestDiameter).toEqual('53.9');
+  });
+
+  it('should get longest and shortest diameters defined even with undefined handles', () => {
+    measurementData.handles = {
+      start: {
+        x: 0,
+        y: 0,
+      },
+      end: {
+        x: 4,
+        y: 4,
+      },
+      perpendicularStart: {},
+      perpendicularEnd: {},
+    };
+
+    calculateLongestAndShortestDiameters(eventData, measurementData);
+
+    expect(measurementData.shortestDiameter).toEqual('0.0');
+    expect(measurementData.longestDiameter).toEqual('5.7');
+  });
+
+  it('should make shortestDiameter always small than longestDiameter', () => {
+    measurementData.handles = {
+      start: {
+        x: 10,
+        y: 10,
+      },
+      end: {
+        x: 5,
+        y: 5,
+      },
+      perpendicularStart: {
+        x: 12,
+        y: 12,
+      },
+      perpendicularEnd: {
+        x: 5,
+        y: 5,
+      },
+    };
+
+    calculateLongestAndShortestDiameters(eventData, measurementData);
+
+    expect(measurementData.shortestDiameter).toEqual('7.1');
+    expect(measurementData.longestDiameter).toEqual('9.9');
   });
 });
