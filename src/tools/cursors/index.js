16cf0a09292c7ed7ceb83944adf2ac5754e506b4
dannyrb <danny.ri.brown@gmail.com>
2019-03-21 20:57:05 -0400

    Shift default export beneath initializations

--- src/tools/cursors/index.js
+++ src/tools/cursors/index.js
@@ -1,28 +1,5 @@
 import MouseCursor from './MouseCursor.js';
 
-export default {
-  angleCursor,
-  arrowAnnotateCursor,
-  bidirectionalCursor,
-  cobbAngleCursor,
-  ellipticalRoiCursor,
-  freehandMouseCursor,
-  freehandSculpterMouseCursor,
-  lengthCursor,
-  probeCursor,
-  rectangleRoiCursor,
-  textMarkerCursor,
-  crosshairsCursor,
-  eraserCursor,
-  magnifyCursor,
-  panCursor,
-  rotateCursor,
-  stackScrollCursor,
-  wwwcRegionCursor,
-  wwwcCursor,
-  zoomCursor,
-};
-
 export const angleCursor = new MouseCursor(
   `<path fill="ACTIVE_COLOR" d="M1203 544q0 13-10 23l-393 393 393 393q10 10 10 23t-10 23l-50
         50q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l466-466q10-10 23-10t23
@@ -379,3 +356,26 @@ export const zoomCursor = new MouseCursor(
     },
   }
 );
+
+export default {
+  angleCursor,
+  arrowAnnotateCursor,
+  bidirectionalCursor,
+  cobbAngleCursor,
+  ellipticalRoiCursor,
+  freehandMouseCursor,
+  freehandSculpterMouseCursor,
+  lengthCursor,
+  probeCursor,
+  rectangleRoiCursor,
+  textMarkerCursor,
+  crosshairsCursor,
+  eraserCursor,
+  magnifyCursor,
+  panCursor,
+  rotateCursor,
+  stackScrollCursor,
+  wwwcRegionCursor,
+  wwwcCursor,
+  zoomCursor,
+};
