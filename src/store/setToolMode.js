1ec8df81f9a259db8990158ee0c5d1af7781e3d2
Mike Hazell <mike.hazell@gmail.com>
2019-03-25 15:14:22 +1100

    Do the de-deuplication without using Set

--- src/store/setToolMode.js
+++ src/store/setToolMode.js
@@ -210,9 +210,15 @@ function setToolModeForElement(mode, changeEvent, element, toolName, options) {
     options.mouseButtonMask.length !== 0 &&
     Array.isArray(tool.options.mouseButtonMask)
   ) {
-    options.mouseButtonMask = Array.from(
-      new Set(options.mouseButtonMask.concat(tool.options.mouseButtonMask))
-    );
+    options.mouseButtonMask = options.mouseButtonMask
+      .concat(tool.options.mouseButtonMask)
+      .reduce((acc, m) => {
+        if (acc.indexOf(m) === -1) {
+          acc.push(m);
+        }
+
+        return acc;
+      }, []);
   }
 
   // Set mode & options
