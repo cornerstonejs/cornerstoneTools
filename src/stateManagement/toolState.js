4f79aac95b1cc1cc93d64bdebc6630dd78c8bc1c
saseokjun <sizning1412@gmail.com>
2019-06-01 16:37:08 +0900

    Modify toolstate compare function for removeToolState method

--- src/stateManagement/toolState.js
+++ src/stateManagement/toolState.js
@@ -3,6 +3,41 @@ import external from '../externalModules.js';
 import { globalImageIdSpecificToolStateManager } from './imageIdSpecificStateManager.js';
 import triggerEvent from '../util/triggerEvent.js';
 
+function compare ( x, y ) {
+  if ( x === y ) return true;
+    // if both x and y are null or undefined and exactly the same
+
+  if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+    // if they are not strictly equal, they both need to be Objects
+
+  if ( x.constructor !== y.constructor ) return false;
+    // they must have the exact same prototype chain, the closest we can do is
+    // test there constructor.
+
+  for ( var p in x ) {
+    if ( ! x.hasOwnProperty( p ) ) continue;
+      // other properties were tested using x.constructor === y.constructor
+
+    if ( ! y.hasOwnProperty( p ) ) return false;
+      // allows to compare x[ p ] and y[ p ] when set to undefined
+
+    if ( x[ p ] === y[ p ] ) continue;
+      // if they have the same strict value or identity then they are equal
+
+    if ( typeof( x[ p ] ) !== "object" ) return false;
+      // Numbers, Strings, Functions, Booleans must be strictly equal
+
+    if ( ! compare( x[ p ],  y[ p ] ) ) return false;
+      // Objects and Arrays must be tested recursively
+  }
+
+  for ( p in y ) {
+    if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+      // allows x[ p ] to be set to undefined
+  }
+  return true;
+}
+
 function getElementToolStateManager (element) {
   const enabledImage = external.cornerstone.getEnabledElement(element);
   // If the enabledImage has no toolStateManager, create a default one for it
@@ -48,7 +83,7 @@ function removeToolState (element, toolType, data) {
   let indexOfData = -1;
 
   for (let i = 0; i < toolData.data.length; i++) {
-    if (toolData.data[i] === data) {
+    if (compare(toolData.data[i], data)) {
       indexOfData = i;
     }
   }
