5b1e995b22a07612f2fcfd72264ca5a64f946ae5
사석준 <saseokjun@MacBookPro.local>
2019-06-11 20:15:25 +0900

    Modify toolstate compare function for removeToolState method

--- src/stateManagement/toolState.js
+++ src/stateManagement/toolState.js
@@ -77,6 +77,42 @@ function getToolState(element, toolType) {
  * @param  {Object} data          The data to remove from the toolStateManager.
  * @returns {undefined}
  */
+
+function compare ( x, y ) {
+  if ( x === y ) return true;
+    // if both x and y are null or undefined and exactly the same
+
+  if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+    // if they are not strictly equal, they both need to be Objects
+
+  if ( x.constructor !== y.constructor ) return false;
+    // they must have the exact same prototype chain, the closest we can do is
+    // test there constructor.
+
+  for ( var p in x ) {
+    if ( ! x.hasOwnProperty( p ) ) continue;
+      // other properties were tested using x.constructor === y.constructor
+
+    if ( ! y.hasOwnProperty( p ) ) return false;
+      // allows to compare x[ p ] and y[ p ] when set to undefined
+
+    if ( x[ p ] === y[ p ] ) continue;
+      // if they have the same strict value or identity then they are equal
+
+    if ( typeof( x[ p ] ) !== "object" ) return false;
+      // Numbers, Strings, Functions, Booleans must be strictly equal
+
+    if ( ! compare( x[ p ],  y[ p ] ) ) return false;
+      // Objects and Arrays must be tested recursively
+  }
+
+  for ( p in y ) {
+    if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+      // allows x[ p ] to be set to undefined
+  }
+  return true;
+}
+
 function removeToolState(element, toolType, data) {
   const toolStateManager = getElementToolStateManager(element);
   const toolData = toolStateManager.get(element, toolType);
@@ -84,7 +120,7 @@ function removeToolState(element, toolType, data) {
   let indexOfData = -1;
 
   for (let i = 0; i < toolData.data.length; i++) {
-    if (toolData.data[i] === data) {
+    if (compare(toolData.data[i], data)) {
       indexOfData = i;
     }
   }
