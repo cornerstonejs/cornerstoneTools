0640fb44bc16ff51b9db1b1797705c3acb7ad489
romulo bordezani <romulobordezani@gmail.com>
2019-07-01 17:55:21 -0300

    Limiting the boundaries to the image width and height.

--- src/tools/brush/utils/Boundaries/getBoundingBoxAroundPolygon.js
+++ src/tools/brush/utils/Boundaries/getBoundingBoxAroundPolygon.js
@@ -1,8 +1,9 @@
-export default function getBoundingBoxAroundPolygon(vertices) {
+export default function getBoundingBoxAroundPolygon(vertices, image) {
   let xMin = Infinity;
   let xMax = 0;
   let yMin = Infinity;
   let yMax = 0;
+  const { width, height } = image;
 
   vertices.forEach(v => {
     xMin = Math.min(v[0], xMin);
@@ -15,6 +16,12 @@ export default function getBoundingBoxAroundPolygon(vertices) {
   yMin = Math.round(yMin);
   xMax = Math.round(xMax);
   yMax = Math.round(yMax);
+
+  xMax = xMax > width ? width : xMax;
+  xMin = xMin < 0 ? 0 : xMin;
+
+  yMax = yMax > height ? height : yMax;
+  yMin = yMin < 0 ? 0 : yMin;
 
   return [[xMin, yMin], [xMax, yMax]];
 }
