a80a3ec6806735b3c09f7c9589fe83637ac91e14
James A. Petts <jamesapetts@Jamess-MacBook-Pro.local>
2019-08-08 16:52:20 +0100

    Clear region if all points on line are within the segment.

--- src/util/segmentation/operations/correction.js
+++ src/util/segmentation/operations/correction.js
@@ -41,30 +41,53 @@ export default function correction(
 
   logger.warn(points);
 
-  const outsideLabelValue = 0;
-  const startAndEndEqual =
-    points[0].segment === points[points.length - 1].segment;
-  const startOutside = points[0].segment === outsideLabelValue;
-  const firstInsidePoint = points.find(p => p.segment !== 0);
+  //const startAndEndEqual =
+  //  points[0].segment === points[points.length - 1].segment;
+  //const startOutside = points[0].segment === outsideLabelValue;
 
-  if (!firstInsidePoint) {
-    logger.warn('The line never intersects a segment');
+  let allInside = true;
+  let allOutside = true;
+
+  for (let i = 0; i < points.length; i++) {
+    const point = points[i];
+
+    if (point.segment === labelValue) {
+      allOutside = false;
+    } else {
+      allInside = false;
+    }
+
+    if (!allInside && !allOutside) {
+      break;
+    }
+  }
+
+  //const firstInsidePoint = points.find(p => p.segment === labelValue);
+
+  logger.warn(allOutside, allInside);
+
+  if (allOutside) {
+    logger.warn('The line never intersects a segment.');
     fillInside(points, segmentationData, evt, labelValue);
 
     return;
   }
 
-  const firstInsideSegment = firstInsidePoint.segment;
+  if (allInside) {
+    logger.warn('The line is only ever inside the segment.');
+    fillInside(points, segmentationData, evt, 0);
 
-  const allSegmentsEqualOrOutside = points.every(
-    p => p.segment === firstInsideSegment || p.segment === 0
-  );
+    return;
+  }
 
-  if (startOutside && startAndEndEqual && allSegmentsEqualOrOutside) {
+  /*
+  if (startOutside && startAndEndEqual ) {
     // If the user draws a line which starts and ends outside the segmenation a part of it is cut off (left image)
     // TODO: this behaviour currently isn't correct. It should erase the entire portion of the segment, not just what is inside the polygon defined by the points
     // Not sure what the fastest way to do this is yet.
-    fillInside(points, segmentationData, evt, outsideLabelValue);
+
+    // TODO -> Don't use zero. I think the fill mechanism in general needs to be rewritten to support multiple segments.
+    fillInside(points, segmentationData, evt, 0);
 
     return;
   } else if (!startOutside && startAndEndEqual && allSegmentsEqualOrOutside) {
@@ -73,4 +96,5 @@ export default function correction(
 
     return;
   }
+  */
 }
