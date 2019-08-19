3b62757b0525366db8371949aa820f63d7275026
James A. Petts <jamesapetts@Jamess-MacBook-Pro.local>
2019-08-19 17:37:20 +0100

    Toggle segment visibility API.

--- src/store/modules/segmentationModule.js
+++ src/store/modules/segmentationModule.js
@@ -247,6 +247,120 @@ function getAndCacheLabelmap2D(elementOrEnabledElementUID) {
 }
 
 /**
+ * GetIsSegmentVisible -  Returns if a segment is visible.
+ *
+ * @param  {HTMLElement|string} elementOrEnabledElementUID   The cornerstone enabled
+ *                                                    element or its UUID.
+ * @param  {number} segmentIndex     The segment index.
+ * @param  {number} [labelmapIndex]    If undefined, defaults to the active
+ *                                     labelmap index.
+ * @returns {boolean} True if the segment is visible.
+ */
+function getIsSegmentVisible(
+  elementOrEnabledElementUID,
+  segmentIndex,
+  labelmapIndex
+) {
+  if (!segmentIndex) {
+    return;
+  }
+
+  const element = _getEnabledElement(elementOrEnabledElementUID);
+
+  if (!element) {
+    return;
+  }
+
+  const stackState = getToolState(element, 'stack');
+  const stackData = stackState.data[0];
+  const firstImageId = stackData.imageIds[0];
+
+  const brushStackState = state.series[firstImageId];
+
+  if (!brushStackState) {
+    logger.warn(`brushStackState is undefined`);
+
+    return;
+  }
+
+  if (labelmapIndex === undefined) {
+    labelmapIndex = brushStackState.activeLabelmapIndex;
+  }
+
+  if (!brushStackState.labelmaps3D[labelmapIndex]) {
+    logger.warn(`No labelmap3D of labelmap index ${labelmapIndex} on stack.`);
+
+    return;
+  }
+
+  const labelmap3D = brushStackState.labelmaps3D[labelmapIndex];
+  const visible = labelmap3D.segmentsVisible[segmentIndex];
+
+  return visible || visible === undefined;
+}
+
+/**
+ * SetToggleSegmentVisibility - Toggles the visability of a segment.
+ *
+ * @param  {HTMLElement|string} elementOrEnabledElementUID   The cornerstone enabled
+ *                                                    element or its UUID.
+ * @param  {number} segmentIndex     The segment index.
+ * @param  {number} [labelmapIndex]    If undefined, defaults to the active
+ *                                     labelmap index.
+ * @returns {boolean} True if the segment is now visible.
+ */
+function setToggleSegmentVisibility(
+  elementOrEnabledElementUID,
+  segmentIndex,
+  labelmapIndex
+) {
+  if (!segmentIndex) {
+    return;
+  }
+
+  const element = _getEnabledElement(elementOrEnabledElementUID);
+
+  if (!element) {
+    return;
+  }
+
+  const stackState = getToolState(element, 'stack');
+  const stackData = stackState.data[0];
+  const firstImageId = stackData.imageIds[0];
+
+  const brushStackState = state.series[firstImageId];
+
+  if (!brushStackState) {
+    logger.warn(`brushStackState is undefined`);
+
+    return;
+  }
+
+  if (labelmapIndex === undefined) {
+    labelmapIndex = brushStackState.activeLabelmapIndex;
+  }
+
+  if (!brushStackState.labelmaps3D[labelmapIndex]) {
+    logger.warn(`No labelmap3D of labelmap index ${labelmapIndex} on stack.`);
+
+    return;
+  }
+
+  const labelmap3D = brushStackState.labelmaps3D[labelmapIndex];
+  const segmentsVisible = labelmap3D.segmentsVisible;
+
+  const visible = segmentsVisible[segmentIndex];
+
+  if (visible || visible === undefined) {
+    segmentsVisible[segmentIndex] = false;
+  } else {
+    segmentsVisible[segmentIndex] = true;
+  }
+
+  return segmentsVisible[segmentIndex];
+}
+
+/**
  * SetCacheLabelMap2DView - Caches a `Labelmap2D` view of a `Labelmap3D`
  * for the given `imageIdIndex` if it doesn't yet exist.
  *
@@ -1133,6 +1247,7 @@ export default {
     labelmaps3D: getLabelmaps3D,
     activeLabelmapIndex: getActiveLabelmapIndex,
     activeSegmentIndex: getActiveSegmentIndex,
+    isSegmentVisible: getIsSegmentVisible,
     getAndCacheLabelmap2D,
     labelmapStats: getLabelmapStats,
     segmentOfActiveLabelmapAtEvent: getSegmentOfActiveLabelmapAtEvent,
@@ -1149,6 +1264,7 @@ export default {
     decrementActiveSegmentIndex: setDecrementActiveSegmentIndex,
     cacheLabelMap2DView: setCacheLabelMap2DView,
     activeSegmentIndex: setActiveSegmentIndex,
+    toggleSegmentVisibility: setToggleSegmentVisibility,
     updateSegmentsOnLabelmaps2D: setUpdateSegmentsOnLabelmaps2D,
     deleteSegment: setDeleteSegment,
     colorLUT: setColorLUT,
