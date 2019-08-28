68b5be111ed0cef60139c39cee6ed0af14ea314a
James A. Petts <jamesapetts@gmail.com>
2019-08-28 15:21:08 +0100

    Finish DRY jobs.

--- src/tools/segmentation/strategies/index.js
+++ src/tools/segmentation/strategies/index.js
@@ -1,9 +1,10 @@
 export { default as correction } from './correction';
-export { default as eraseInsideFreehand } from './eraseInsideFreehand';
-export { default as eraseInsideRectangle } from './eraseInsideRectangle.js';
-export { default as eraseOutsideFreehand } from './eraseOutsideFreehand';
+export { eraseInsideFreehand, eraseOutsideFreehand } from './eraseFreehand';
+export {
+  eraseInsideRectangle,
+  eraseOutsideRectangle,
+} from './eraseRectangle.js';
 export { fillInsideFreehand, fillOutsideFreehand } from './fillFreehand';
 export { fillInsideRectangle, fillOutsideRectangle } from './fillRectangle';
 export { fillInsideCircle, fillOutsideCircle } from './fillCircle';
-export { default as eraseOutsideRectangle } from './eraseOutsideRectangle';
 export { eraseInsideCircle, eraseOutsideCircle } from './eraseCircle';
