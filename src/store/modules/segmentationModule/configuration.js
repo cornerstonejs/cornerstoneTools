6eff45d67ae1f161551268ca7a1f8197e0339b64
James A. Petts <jamesapetts@gmail.com>
2019-08-30 10:03:23 +0100

    Revert duck back to Human.

--- src/store/modules/segmentationModule/configuration.js
+++ src/store/modules/segmentationModule/configuration.js
@@ -3,7 +3,7 @@ const configuration = {
   renderOutline: true,
   renderFill: true,
   shouldRenderInactiveLabelmaps: true,
-  radius: 100,
+  radius: 10,
   minRadius: 1,
   maxRadius: 50,
   segmentsPerLabelmap: 65535, // Max is 65535 due to using 16-bit Unsigned ints.
