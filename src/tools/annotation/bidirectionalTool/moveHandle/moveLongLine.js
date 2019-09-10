7b98df04d84d35b313266669ac123c3883538913
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2019-09-10 09:35:21 -0300

    Reducing code complexity from moveLongLine method

--- src/tools/annotation/bidirectionalTool/moveHandle/moveLongLine.js
+++ src/tools/annotation/bidirectionalTool/moveHandle/moveLongLine.js
@@ -2,17 +2,31 @@ import getDistance from './getDistance.js';
 import getLineVector from './getLineVector.js';
 import getBaseData from './getBaseData.js';
 
+function updateLine(cps, rps, intersection, fixedPoint, mid, data) {
+  const { start, perpendicularStart, perpendicularEnd } = data.handles;
+
+  // Get the original distance from perpendicular handles to intersection
+  const distancePS = getDistance(cps, rps, perpendicularStart, intersection);
+  const distancePE = getDistance(cps, rps, perpendicularEnd, intersection);
+
+  // Inclination of the perpendicular line
+  const vector = getLineVector(cps, rps, fixedPoint, mid);
+
+  // Define the multipliers
+  const mult1 = fixedPoint === start ? 1 : -1;
+  const mult2 = mult1 * -1;
+
+  // Calculate and set the new position of the perpendicular handles
+  perpendicularStart.x = mid.x + vector.y * distancePS * rps * mult1;
+  perpendicularStart.y = mid.y + vector.x * distancePS * cps * mult2;
+  perpendicularEnd.x = mid.x + vector.y * distancePE * rps * mult2;
+  perpendicularEnd.y = mid.y + vector.x * distancePE * cps * mult1;
+}
+
 // Move long line handle
 export default function(proposedPoint, data, eventData, fixedPoint) {
-  const {
-    cps,
-    rps,
-    start,
-    perpendicularEnd,
-    perpendicularStart,
-    intersection,
-    distanceToFixed,
-  } = getBaseData(data, eventData, fixedPoint);
+  const baseData = getBaseData(data, eventData, fixedPoint);
+  const { cps, rps, intersection, distanceToFixed } = baseData;
 
   const newLineLength = getDistance(cps, rps, fixedPoint, proposedPoint);
 
@@ -28,24 +42,8 @@ export default function(proposedPoint, data, eventData, fixedPoint) {
     y: fixedPoint.y + (proposedPoint.y - fixedPoint.y) * k,
   };
 
-  // Get the original distance from perpendicular handles to intersection
-  const distancePS = getDistance(cps, rps, perpendicularStart, intersection);
-  const distancePE = getDistance(cps, rps, perpendicularEnd, intersection);
-
-  // Inclination of the perpendicular line
-  const vector = getLineVector(cps, rps, fixedPoint, newIntersection);
-
-  // Define the mid points and the multipliers
-  const midX = newIntersection.x;
-  const midY = newIntersection.y;
-  const mult1 = fixedPoint === start ? 1 : -1;
-  const mult2 = mult1 * -1;
-
   // Calculate and set the new position of the perpendicular handles
-  perpendicularStart.x = midX + vector.y * distancePS * rps * mult1;
-  perpendicularStart.y = midY + vector.x * distancePS * cps * mult2;
-  perpendicularEnd.x = midX + vector.y * distancePE * rps * mult2;
-  perpendicularEnd.y = midY + vector.x * distancePE * cps * mult1;
+  updateLine(cps, rps, intersection, fixedPoint, newIntersection, data);
 
   return true;
 }
