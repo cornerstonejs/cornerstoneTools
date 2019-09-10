cce764ba2d84799f7e1b5a131aeb16408ea4747e
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2019-09-10 15:58:49 -0300

    Reducing complexity of getBaseData method

--- src/tools/annotation/bidirectionalTool/moveHandle/getBaseData.js
+++ src/tools/annotation/bidirectionalTool/moveHandle/getBaseData.js
@@ -1,20 +1,21 @@
 import external from '../../../../externalModules.js';
 import getDistance from './getDistanceWithPixelSpacing.js';
 
+function createLine(startPoint, endPoint) {
+  return {
+    start: startPoint,
+    end: endPoint,
+  };
+}
+
 export default function getBaseData(data, eventData, fixedPoint) {
   const { lineSegment } = external.cornerstoneMath;
   const { start, end, perpendicularStart, perpendicularEnd } = data.handles;
   const { columnPixelSpacing = 1, rowPixelSpacing = 1 } = eventData.image;
 
-  const longLine = {
-    start,
-    end,
-  };
-
-  const intersection = lineSegment.intersectLine(longLine, {
-    start: perpendicularStart,
-    end: perpendicularEnd,
-  });
+  const longLine = createLine(start, end);
+  const perpendicularLine = createLine(perpendicularStart, perpendicularEnd);
+  const intersection = lineSegment.intersectLine(longLine, perpendicularLine);
 
   const distanceToFixed = getDistance(
     columnPixelSpacing,
