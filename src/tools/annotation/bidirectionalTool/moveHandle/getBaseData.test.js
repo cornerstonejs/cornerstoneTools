c3d547b0ac7a7296b55d1107680d3faded2c08f4
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2019-09-17 10:27:53 -0300

    Improving tests based on PR suggestions

--- src/tools/annotation/bidirectionalTool/moveHandle/getBaseData.test.js
+++ src/tools/annotation/bidirectionalTool/moveHandle/getBaseData.test.js
@@ -23,7 +23,7 @@ function createPoint(x, y) {
 }
 
 describe('getBaseData.js', () => {
-  it('base data is extracted with all attributes', () => {
+  it('returns a baseData object with the correct values', () => {
     const start = createPoint(0, 4);
     const end = createPoint(8, 4);
     const perpendicularStart = createPoint(4, 6);
@@ -51,19 +51,26 @@ describe('getBaseData.js', () => {
 
     expect(result.columnPixelSpacing).toEqual(3);
     expect(result.rowPixelSpacing).toEqual(2);
-    expect(result.start).toEqual(start);
-    expect(result.end).toEqual(end);
-    expect(result.perpendicularStart).toEqual(perpendicularStart);
-    expect(result.perpendicularEnd).toEqual(perpendicularEnd);
-    expect(result.longLine.start).toEqual(start);
-    expect(result.longLine.end).toEqual(end);
+    expect(result.start.x).toEqual(0);
+    expect(result.start.y).toEqual(4);
+    expect(result.end.x).toEqual(8);
+    expect(result.end.y).toEqual(4);
+    expect(result.perpendicularStart.x).toEqual(4);
+    expect(result.perpendicularStart.y).toEqual(6);
+    expect(result.perpendicularEnd.x).toEqual(4);
+    expect(result.perpendicularEnd.y).toEqual(2);
+    expect(result.longLine.start.x).toEqual(0);
+    expect(result.longLine.start.y).toEqual(4);
+    expect(result.longLine.end.x).toEqual(8);
+    expect(result.longLine.end.y).toEqual(4);
     expect(result.intersection.x).toEqual(4);
     expect(result.intersection.y).toEqual(4);
     expect(result.distanceToFixed).toEqual(2);
-    expect(result.fixedPoint).toEqual(fixedPoint);
+    expect(result.fixedPoint.x).toEqual(8);
+    expect(result.fixedPoint.y).toEqual(4);
   });
 
-  it('base data is extracted with row and column pixel spacing defaulted to 1', () => {
+  it('baseData object is returned with row and column pixel spacing defaulted to 1', () => {
     const start = createPoint(0, 4);
     const end = createPoint(8, 4);
     const perpendicularStart = createPoint(4, 6);
@@ -88,15 +95,22 @@ describe('getBaseData.js', () => {
 
     expect(result.columnPixelSpacing).toEqual(1);
     expect(result.rowPixelSpacing).toEqual(1);
-    expect(result.start).toEqual(start);
-    expect(result.end).toEqual(end);
-    expect(result.perpendicularStart).toEqual(perpendicularStart);
-    expect(result.perpendicularEnd).toEqual(perpendicularEnd);
-    expect(result.longLine.start).toEqual(start);
-    expect(result.longLine.end).toEqual(end);
+    expect(result.start.x).toEqual(0);
+    expect(result.start.y).toEqual(4);
+    expect(result.end.x).toEqual(8);
+    expect(result.end.y).toEqual(4);
+    expect(result.perpendicularStart.x).toEqual(4);
+    expect(result.perpendicularStart.y).toEqual(6);
+    expect(result.perpendicularEnd.x).toEqual(4);
+    expect(result.perpendicularEnd.y).toEqual(2);
+    expect(result.longLine.start.x).toEqual(0);
+    expect(result.longLine.start.y).toEqual(4);
+    expect(result.longLine.end.x).toEqual(8);
+    expect(result.longLine.end.y).toEqual(4);
     expect(result.intersection.x).toEqual(4);
     expect(result.intersection.y).toEqual(4);
     expect(result.distanceToFixed).toEqual(4);
-    expect(result.fixedPoint).toEqual(fixedPoint);
+    expect(result.fixedPoint.x).toEqual(8);
+    expect(result.fixedPoint.y).toEqual(4);
   });
 });
