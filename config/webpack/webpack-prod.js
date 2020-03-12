5d36dc370a3769fbb5a1170038c52a900584af90
Betalos Oralyos <thoura.oussama@gmail.com>
2020-03-12 18:56:15 +0100

    fix production build (fixes #941)

--- config/webpack/webpack-prod.js
+++ config/webpack/webpack-prod.js
@@ -1,19 +1,22 @@
-const webpack = require('webpack');
+const path = require('path');
 const merge = require('./merge');
 const baseConfig = require('./webpack-base');
 const TerserPlugin = require('terser-webpack-plugin');
 
+const rootPath = process.cwd();
+const context = path.join(rootPath, 'src');
+
 const prodConfig = {
-  output: {
-    filename: '[name].min.js',
+  entry: {
+    'cornerstoneTools.min': path.join(context, 'index.js'),
   },
-  mode: 'production',
   optimization: {
     minimizer: [
       new TerserPlugin({
         cache: true,
         parallel: true,
         sourceMap: true, // Must be set to true if using source-maps in production
+        test: /\.min\.js$/, // Minimize only .min one
         terserOptions: {
           // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
           compress: {
@@ -23,11 +26,7 @@ const prodConfig = {
       }),
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-  ],
+  plugins: [],
 };
 
 module.exports = merge(baseConfig, prodConfig);
