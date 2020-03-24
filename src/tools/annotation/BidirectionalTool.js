35c352d72adac8c56bf918c5e11e7c48423f29a6
dannyrb <danny.ri.brown@gmail.com>
2020-03-24 15:28:56 -0400

    chore: simplify pointNearTool to use same logic as length tool for both line segments

--- src/tools/annotation/BidirectionalTool.js
+++ src/tools/annotation/BidirectionalTool.js
@@ -1,7 +1,5 @@
 import BaseAnnotationTool from '../base/BaseAnnotationTool.js';
 
-import createNewMeasurement from './bidirectionalTool/createNewMeasurement.js';
-import pointNearTool from './bidirectionalTool/pointNearTool.js';
 import renderToolData from './bidirectionalTool/renderToolData.js';
 import addNewMeasurement from './bidirectionalTool/addNewMeasurement.js';
 import _moveCallback from './bidirectionalTool/mouseMoveCallback.js';
@@ -12,6 +10,9 @@ import { bidirectionalCursor } from '../cursors/index.js';
 import throttle from '../../util/throttle';
 import getPixelSpacing from '../../util/getPixelSpacing';
 import calculateLongestAndShortestDiameters from './bidirectionalTool/utils/calculateLongestAndShortestDiameters';
+//
+import { state } from '../../store/index.js';
+import lineSegDistance from './../../util/lineSegDistance.js';
 
 const emptyLocationCallback = (measurementData, eventData, doneCallback) =>
   doneCallback();
@@ -46,8 +47,6 @@ export default class BidirectionalTool extends BaseAnnotationTool {
 
     this.throttledUpdateCachedStats = throttle(this.updateCachedStats, 110);
 
-    this.createNewMeasurement = createNewMeasurement.bind(this);
-    this.pointNearTool = pointNearTool.bind(this);
     this.renderToolData = renderToolData.bind(this);
     this.addNewMeasurement = addNewMeasurement.bind(this);
     this._moveCallback = _moveCallback.bind(this);
@@ -55,6 +54,70 @@ export default class BidirectionalTool extends BaseAnnotationTool {
     this.handleSelectedCallback = handleSelectedCallback.bind(this);
     this.handleSelectedMouseCallback = handleSelectedMouseCallback.bind(this);
     this.handleSelectedTouchCallback = handleSelectedTouchCallback.bind(this);
+  }
+
+  createNewMeasurement(mouseEventData) {
+    const { x, y } = mouseEventData.currentPoints.image;
+    // Create the measurement data for this tool with the end handle activated
+    const measurementData = {
+      toolType: this.name,
+      isCreating: true,
+      visible: true,
+      active: true,
+      invalidated: true,
+      handles: {
+        start: _getHandle(x, y, 0),
+        end: _getHandle(x, y, 1, { active: true }),
+        perpendicularStart: _getHandle(x, y, 2, { locked: true }),
+        perpendicularEnd: _getHandle(x, y, 3),
+        textBox: _getHandle(x - 50, y - 70, null, {
+          highlight: false,
+          hasMoved: true,
+          active: false,
+          movesIndependently: false,
+          drawnIndependently: true,
+          allowedOutsideImage: true,
+          hasBoundingBox: true,
+        }),
+      },
+      longestDiameter: 0,
+      shortestDiameter: 0,
+    };
+
+    return measurementData;
+  }
+
+  /**
+   *
+   * @param {*} element
+   * @param {*} annotation
+   * @param {*} coords
+   * @param {*} interactionType
+   */
+  pointNearTool(element, annotation, coords, interactionType = 'mouse') {
+    if (!annotation || !annotation.handles || annotation.visible === false) {
+      return false;
+    }
+
+    const distanceThreshold =
+      interactionType === 'mouse' ? state.clickProximity : state.touchProximity;
+    const {
+      start,
+      end,
+      perpendicularStart,
+      perpendicularEnd,
+    } = annotation.handles;
+    const isNearLine =
+      start &&
+      end &&
+      lineSegDistance(element, start, end, coords) < distanceThreshold;
+    const isNearPerpendicularLine =
+      perpendicularStart &&
+      perpendicularEnd &&
+      lineSegDistance(element, perpendicularStart, perpendicularEnd, coords) <
+        distanceThreshold;
+
+    return isNearLine || isNearPerpendicularLine;
   }
 
   updateCachedStats(image, element, data) {
@@ -70,3 +133,17 @@ export default class BidirectionalTool extends BaseAnnotationTool {
     data.invalidated = false;
   }
 }
+
+const _getHandle = (x, y, index, extraAttributes = {}) =>
+  Object.assign(
+    {
+      x,
+      y,
+      index,
+      drawnIndependently: false,
+      allowedOutsideImage: false,
+      highlight: true,
+      active: false,
+    },
+    extraAttributes
+  );
