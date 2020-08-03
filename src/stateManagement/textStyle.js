ce69b304d03b1ba12df7833d33fac884d2498f06
Bruno Alves de Faria <bruno.faria@radicalimaging.com>
2020-08-03 20:57:37 -0300

    Fixing textStyle API inconsistency between font size and font style

--- src/stateManagement/textStyle.js
+++ src/stateManagement/textStyle.js
@@ -1,38 +1,56 @@
-let defaultFontSize = 15,
-  defaultFont = `${defaultFontSize}px Arial`,
-  defaultBackgroundColor = 'transparent';
+const textStyle = {
+  fontSize: 15,
+  fontFamily: 'Arial',
+  backgroundColor: 'transparent',
+};
 
+// Deprecate
 function setFont(font) {
-  defaultFont = font;
+  const split = font.split('px ');
+
+  if (split.length === 2) {
+    setFontSize(split[0]);
+    setFontFamily(split[1]);
+  }
 }
 
 function getFont() {
-  return defaultFont;
+  return `${textStyle.fontSize}px ${textStyle.fontFamily}`;
+}
+
+function setFontFamily(fontFamily) {
+  textStyle.fontFamily = fontFamily;
+}
+
+function getFontFamily() {
+  return textStyle.fontFamily;
 }
 
 function setFontSize(fontSize) {
-  defaultFontSize = fontSize;
+  textStyle.fontSize = parseFloat(fontSize);
 }
 
 function getFontSize() {
-  return defaultFontSize;
+  return textStyle.fontSize;
 }
 
 function setBackgroundColor(backgroundColor) {
-  defaultBackgroundColor = backgroundColor;
+  textStyle.backgroundColor = backgroundColor;
 }
 
 function getBackgroundColor() {
-  return defaultBackgroundColor;
+  return textStyle.backgroundColor;
 }
 
-const textStyle = {
+const textStyleApi = {
   setFont,
   getFont,
   setFontSize,
   getFontSize,
+  setFontFamily,
+  getFontFamily,
   setBackgroundColor,
   getBackgroundColor,
 };
 
-export default textStyle;
+export default textStyleApi;
