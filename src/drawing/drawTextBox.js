3522dd373a0eced4bc0db7c920ba24b37c492def
Bruno Alves de Faria <bruno.faria@radicalimaging.com>
2020-08-04 09:59:32 -0300

    Adding bounding box's translator option for drawing text boxes

--- src/drawing/drawTextBox.js
+++ src/drawing/drawTextBox.js
@@ -44,7 +44,7 @@ export function textBoxWidth(context, text, padding) {
  * @param  {Object} options     Options for the textBox.
  * @returns {Object} {top, left, width, height} - Bounding box; can be used for pointNearTool
  */
-export default function(context, textLines, x, y, color, options) {
+export default function(context, textLines, x, y, color, options = {}) {
   if (Object.prototype.toString.call(textLines) !== '[object Array]') {
     textLines = [textLines];
   }
@@ -74,21 +74,26 @@ export default function(context, textLines, x, y, color, options) {
     context.strokeStyle = color;
 
     // Draw the background box with padding
-    if (options && options.centering && options.centering.x === true) {
-      x -= boundingBox.width / 2;
-    }
+    if (options.centering) {
+      // Draw the background box with padding
+      if (options.centering.x === true) {
+        x -= boundingBox.width / 2;
+      }
 
-    if (options && options.centering && options.centering.y === true) {
-      y -= boundingBox.height / 2;
+      if (options.centering.y === true) {
+        y -= boundingBox.height / 2;
+      }
     }
 
     boundingBox.left = x;
     boundingBox.top = y;
 
-    const fillStyle =
-      options && options.debug === true ? '#FF0000' : backgroundColor;
+    // Check if a translator function was provided
+    if (typeof options.translator === 'function') {
+      options.translator(boundingBox);
+    }
 
-    fillBox(context, boundingBox, fillStyle);
+    fillBox(context, boundingBox, backgroundColor);
 
     // Draw each of the text lines on top of the background box
     fillTextLines(context, boundingBox, textLines, color, padding);
