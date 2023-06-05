00e1fe3b0f087b0bd615f8697d482641c4930e22
Douglas Yau <dy@dicomserver.net>
2023-06-05 16:19:12 -0600

    Correctly scale the adding of the handles

--- src/tools/annotation/bidirectionalTool/utils/updatePerpendicularLineHandles.js
+++ src/tools/annotation/bidirectionalTool/utils/updatePerpendicularLineHandles.js
@@ -22,30 +22,33 @@ export default function updatePerpendicularLineHandles(
 
   const { start, end } = measurementData.handles;
   const { columnPixelSpacing = 1, rowPixelSpacing = 1 } = eventData.image;
+  const { scaledmageFactor = 1 } = eventData.image.imageFrame;
 
   if (start.x === end.x && start.y === end.y) {
-    startX = start.x;
-    startY = start.y;
-    endX = end.x;
-    endY = end.y;
+    startX = start.originalX;
+    startY = start.originalY;
+    endX = end.originalX;
+    endY = end.originalY;
   } else {
     // Mid point of long-axis line
     const mid = {
-      x: (start.x + end.x) / 2,
-      y: (start.y + end.y) / 2,
+      x: (start.originalX + end.originalX) / 2,
+      y: (start.originalY + end.originalY) / 2,
     };
 
     // Inclination of the perpendicular line
     const vector = getLineVector(
-      columnPixelSpacing,
-      rowPixelSpacing,
+      columnPixelSpacing * scaledmageFactor,
+      rowPixelSpacing * scaledmageFactor,
       start,
       end
     );
 
     const perpendicularLineLength = vector.length / 2;
-    const rowMultiplier = perpendicularLineLength / (2 * rowPixelSpacing);
-    const columnMultiplier = perpendicularLineLength / (2 * columnPixelSpacing);
+    const rowMultiplier =
+      perpendicularLineLength / (2 * rowPixelSpacing * scaledmageFactor);
+    const columnMultiplier =
+      perpendicularLineLength / (2 * columnPixelSpacing * scaledmageFactor);
 
     startX = mid.x + columnMultiplier * vector.y;
     startY = mid.y - rowMultiplier * vector.x;
@@ -53,10 +56,10 @@ export default function updatePerpendicularLineHandles(
     endY = mid.y + rowMultiplier * vector.x;
   }
 
-  measurementData.handles.perpendicularStart.x = startX;
-  measurementData.handles.perpendicularStart.y = startY;
-  measurementData.handles.perpendicularEnd.x = endX;
-  measurementData.handles.perpendicularEnd.y = endY;
+  measurementData.handles.perpendicularStart.originalX = startX;
+  measurementData.handles.perpendicularStart.originalY = startY;
+  measurementData.handles.perpendicularEnd.originalX = endX;
+  measurementData.handles.perpendicularEnd.originalY = endY;
 
   return true;
 }
