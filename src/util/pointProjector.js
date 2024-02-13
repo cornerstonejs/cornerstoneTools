e996bb7c67ec7ebcc2a8c9ec6aebbbeb9f2446d6
Amar Tabucic <amar.tabucic@dedalus.eu>
2024-02-13 17:01:32 +0100

    DVISA-2010: (fix) reference line
    Signed-off-by: Amar Tabucic <amar.tabucic@dedalus.eu>

--- src/util/pointProjector.js
+++ src/util/pointProjector.js
@@ -192,10 +192,93 @@ export function planePlaneIntersection(targetImagePlane, referenceImagePlane) {
 
   line.start = origin;
   line.end = origin.clone().add(direction.multiplyScalar(distance));
-
-  // Find the intersections between this line and the reference image plane's four sides
   const rect = getRectangleFromImagePlane(referenceImagePlane);
-  const intersections = lineRectangleIntersection(line, rect);
+  const u = rect.bottom.end
+    .clone()
+    .sub(rect.bottom.start)
+    .normalize();
+  const v = rect.left.end
+    .clone()
+    .sub(rect.left.start)
+    .normalize();
+  const w = u
+    .clone()
+    .cross(v)
+    .normalize();
+
+  const mat4 = new external.cornerstoneMath.Matrix4(
+    u.x,
+    u.y,
+    u.z,
+    0,
+    v.x,
+    v.y,
+    v.z,
+    0,
+    w.x,
+    w.y,
+    w.z,
+    0,
+    0,
+    0,
+    0,
+    1
+  );
+  const mat4T = new external.cornerstoneMath.Matrix4(
+    u.x,
+    v.x,
+    w.x,
+    0,
+    u.y,
+    v.y,
+    w.y,
+    0,
+    u.z,
+    v.z,
+    w.z,
+    0,
+    0,
+    0,
+    0,
+    1
+  );
+  const aabbBottomLeft = imagePointToPatientPoint(
+    {
+      x: 0,
+      y: 0,
+    },
+    referenceImagePlane
+  )
+    .clone()
+    .applyMatrix4(mat4);
+  const aabbTopRight = imagePointToPatientPoint(
+    {
+      x: referenceImagePlane.columns,
+      y: referenceImagePlane.rows,
+    },
+    referenceImagePlane
+  )
+    .clone()
+    .applyMatrix4(mat4);
+
+  const originTransf = origin.clone().applyMatrix4(mat4);
+  const directionTransf = line.end
+    .sub(line.start)
+    .clone()
+    .applyMatrix4(mat4);
+
+  const intersectionsTransf = intersectRayAABB(
+    originTransf.toArray(),
+    directionTransf.toArray(),
+    [aabbBottomLeft.toArray(), aabbTopRight.toArray()],
+    -1000,
+    1000
+  );
+
+  const intersections = [
+    intersectionsTransf[0].clone().applyMatrix4(mat4T),
+    intersectionsTransf[1].clone().applyMatrix4(mat4T),
+  ];
 
   // Return the intersections between this line and the reference image plane's sides
   // In order to draw the reference line from the target image.
@@ -207,6 +290,60 @@ export function planePlaneIntersection(targetImagePlane, referenceImagePlane) {
     start: intersections[0],
     end: intersections[1],
   };
+}
+
+function intersectRayAABB(
+  startPoint,
+  direction,
+  aabb,
+  minDistance,
+  maxDistance
+) {
+  // From Ericson, C. (2005). Real-Time Collision Detection (pp. 179-181). Morgan Kaufmann;
+  // note the respective errata for t_max/t2: http://realtimecollisiondetection.net/books/rtcd/errata/
+  let tMin = minDistance;
+  let tMax = maxDistance;
+
+  for (let i = 0; i < 2; i++) {
+    if (Math.abs(direction[i]) === 0.0) {
+      if (startPoint[i] < aabb[0][i] || startPoint[i] > aabb[1][i]) {
+        console.log(
+          'direction parallel to edge and starting point outside of aabb => returning'
+        );
+
+        return null;
+      }
+    } else {
+      const ood = 1.0 / direction[i];
+      let t1 = (aabb[0][i] - startPoint[i]) * ood;
+      let t2 = (aabb[1][i] - startPoint[i]) * ood;
+
+      if (t1 > t2) {
+        [t1, t2] = [t2, t1]; // Swapping values
+      }
+
+      tMin = Math.max(tMin, t1);
+      tMax = Math.min(tMax, t2);
+
+      if (tMin > tMax) {
+        return null;
+      }
+    }
+  }
+
+  return [
+    new external.cornerstoneMath.Vector3(
+      startPoint[0] + direction[0] * tMin,
+      startPoint[1] + direction[1] * tMin,
+      startPoint[2] + direction[2] * tMin
+    ),
+
+    new external.cornerstoneMath.Vector3(
+      startPoint[0] + direction[0] * tMax,
+      startPoint[1] + direction[1] * tMax,
+      startPoint[2] + direction[2] * tMax
+    ),
+  ];
 }
 
 /**
